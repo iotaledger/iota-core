b931c57fa2c8d10e77a591267641da1a1c12678d
Daria Dziubałtowska <daria305@gmail.com>
2023-04-11 09:19:28 +0200

    Add routes according to the swagger file.

--- components/coreapi/component.go
+++ components/coreapi/component.go
@@ -9,12 +9,101 @@ import (
 	"github.com/iotaledger/hive.go/app"
 	"github.com/iotaledger/inx-app/pkg/httpserver"
 	"github.com/iotaledger/iota-core/components/restapi"
+	restapipkg "github.com/iotaledger/iota-core/pkg/restapi"
 )
 
 const (
 	// RouteInfo is the route for getting the node info.
 	// GET returns the node info.
 	RouteInfo = "/info"
+
+	// RouteBlockIssuance is the route for getting all needed information for block creation.
+	// GET returns the data needed toa attach block.
+	RouteBlockIssuance = "/issuance/block-header"
+
+	// RouteBlock is the route for getting a block by its blockID.
+	// GET returns the block based on the given type in the request "Accept" header.
+	// MIMEApplicationJSON => json.
+	// MIMEVendorIOTASerializer => bytes.
+	RouteBlock = "/blocks/:" + restapipkg.ParameterBlockID
+
+	// RouteBlockMetadata is the route for getting block metadata by its blockID.
+	// GET returns block metadata.
+	RouteBlockMetadata = "/blocks/:" + restapipkg.ParameterBlockID + "/metadata"
+
+	// RouteBlocks is the route for creating new blocks.
+	// POST creates a single new block and returns the new block ID.
+	// The block is parsed based on the given type in the request "Content-Type" header.
+	// By providing only the protocolVersion and payload transaction user can POST a transaction.
+	// MIMEApplicationJSON => json.
+	// MIMEVendorIOTASerializer => bytes.
+	RouteBlocks = "/blocks"
+
+	// RouteOutput is the route for getting an output by its outputID (transactionHash + outputIndex).
+	// GET returns the output based on the given type in the request "Accept" header.
+	// MIMEApplicationJSON => json.
+	// MIMEVendorIOTASerializer => bytes.
+	RouteOutput = "/outputs/:" + restapipkg.ParameterOutputID
+
+	// RouteOutputMetadata is the route for getting output metadata by its outputID (transactionHash + outputIndex) without getting the data again.
+	// GET returns the output metadata.
+	RouteOutputMetadata = "/outputs/:" + restapipkg.ParameterOutputID + "/metadata"
+
+	// RouteTransactionsIncludedBlock is the route for getting the block that was included in the ledger for a given transaction ID.
+	// GET returns the block based on the given type in the request "Accept" header.
+	// MIMEApplicationJSON => json.
+	// MIMEVendorIOTASerializer => bytes.
+	RouteTransactionsIncludedBlock = "/transactions/:" + restapipkg.ParameterTransactionID + "/included-block"
+
+	// RouteTransactionsIncludedBlockMetadata is the route for getting the block metadata that was included in the ledger for a given transaction ID.
+	// GET returns block metadata (including info about "promotion/reattachment needed").
+	RouteTransactionsIncludedBlockMetadata = "/transactions/:" + restapipkg.ParameterTransactionID + "/included-block/metadata"
+
+	// RouteCommitmentByID is the route for getting a slot commitment by its ID.
+	// GET returns the commitment.
+	// MIMEApplicationJSON => json.
+	// MIMEVendorIOTASerializer => bytes.
+	RouteCommitmentByID = "/commitment/:" + restapipkg.ParameterCommitmentID
+
+	// RouteCommitmentByIDUTXOChanges is the route for getting all UTXO changes of a milestone by its ID.
+	// GET returns the output IDs of all UTXO changes.
+	RouteCommitmentByIDUTXOChanges = "/commitment/:" + restapipkg.ParameterCommitmentID + "/utxo-changes"
+
+	// RouteSlotByIndex is the route for getting a milestone by its milestoneIndex.
+	// GET returns the milestone.
+	// MIMEApplicationJSON => json.
+	// MIMEVendorIOTASerializer => bytes.
+	RouteSlotByIndex = "/commitment/by-index/:" + restapipkg.ParameterSlotIndex
+
+	// RouteSlotByIndexUTXOChanges is the route for getting all UTXO changes of a milestone by its milestoneIndex.
+	// GET returns the output IDs of all UTXO changes.
+	RouteSlotByIndexUTXOChanges = "/commitment/by-index/:" + restapipkg.ParameterSlotIndex + "/utxo-changes"
+
+	// RouteAccountsByAcciuntID is the route for getting an account by its accountID.
+	// GET returns the account details.
+	RouteAccountsByAcciuntID = "/accounts/:" + restapipkg.ParameterAccountID
+
+	// RouteAccountMana is the route for getting an account mana by its accountID.
+	// GET returns the account mana details.
+	RouteAccountMana = "/accounts/:" + restapipkg.ParameterAccountID + "/mana"
+
+	// RoutePeer is the route for getting peers by their peerID.
+	// GET returns the peer
+	// DELETE deletes the peer.
+	RoutePeer = "/peers/:" + restapipkg.ParameterPeerID
+
+	// RoutePeers is the route for getting all peers of the node.
+	// GET returns a list of all peers.
+	// POST adds a new peer.
+	RoutePeers = "/peers"
+
+	// RouteControlDatabasePrune is the control route to manually prune the database.
+	// POST prunes the database.
+	RouteControlDatabasePrune = "/control/database/prune"
+
+	// RouteControlSnapshotsCreate is the control route to manually create a snapshot files.
+	// POST creates a full snapshot.
+	RouteControlSnapshotsCreate = "/control/snapshots/create"
 )
 
 func init() {
