3fa441ed66fc0d4850342f3056c3ad651883bd86
Alexander Sporn <github@alexsporn.de>
2023-04-19 08:41:17 +0200

    Fix option name

--- pkg/protocol/engine/consensus/blockgadget/tresholdblockgadget/options.go
+++ pkg/protocol/engine/consensus/blockgadget/tresholdblockgadget/options.go
@@ -2,7 +2,7 @@ package tresholdblockgadget
 
 import "github.com/iotaledger/hive.go/runtime/options"
 
-func WithMarkerAcceptanceThreshold(acceptanceThreshold float64) options.Option[Gadget] {
+func WithAcceptanceThreshold(acceptanceThreshold float64) options.Option[Gadget] {
 	return func(gadget *Gadget) {
 		gadget.optsAcceptanceThreshold = acceptanceThreshold
 	}
