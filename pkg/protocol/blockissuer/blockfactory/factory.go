b471caaac266d1eff8c6236c50a43f86a025aae0
Andrea V <1577639+karimodm@users.noreply.github.com>
2023-04-24 15:58:44 +0200

    Initial blockissuer port

---
+++ pkg/protocol/blockissuer/blockfactory/factory.go
@@ -0,0 +1,177 @@
+package blockfactory
+
+import (
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/iotaledger/hive.go/crypto/ed25519"
+	"github.com/iotaledger/hive.go/runtime/options"
+	"github.com/iotaledger/iota-core/pkg/model"
+	"github.com/iotaledger/iota-core/pkg/protocol/engine/blocks"
+	"github.com/iotaledger/iota-core/pkg/protocol/tipmanager"
+	iotago "github.com/iotaledger/iota.go/v4"
+	"github.com/iotaledger/iota.go/v4/builder"
+)
+
+// region Factory ///////////////////////////////////////////////////////////////////////////////////////////////
+
+// Factory acts as a factory to create new blocks.
+type Factory struct {
+	Events *Events
+
+	// referenceProvider *ReferenceProvider
+	localPrivateKey ed25519.PrivateKey
+	api             iotago.API
+	blockRetriever  func(blockID iotago.BlockID) (block *blocks.Block, exists bool)
+	tipManager      tipmanager.TipManager
+	referencesFunc  ReferencesFunc
+	commitmentFunc  CommitmentFunc
+
+	optsTipSelectionTimeout       time.Duration
+	optsTipSelectionRetryInterval time.Duration
+}
+
+// NewBlockFactory creates a new block factory.
+func NewBlockFactory(localPrivateKey ed25519.PrivateKey, tipManager tipmanager.TipManager, referencesFunc ReferencesFunc, commitmentFunc CommitmentFunc, opts ...options.Option[Factory]) *Factory {
+	return options.Apply(&Factory{
+		Events:          newEvents(),
+		localPrivateKey: localPrivateKey,
+		tipManager:      tipManager,
+		referencesFunc:  referencesFunc,
+		commitmentFunc:  commitmentFunc,
+
+		optsTipSelectionTimeout:       10 * time.Second,
+		optsTipSelectionRetryInterval: 200 * time.Millisecond,
+	}, opts)
+}
+
+// CreateBlock creates a new block including sequence number and tip selection and returns it.
+func (f *Factory) CreateBlock(p iotago.Payload, parentsCount ...int) (*model.Block, error) {
+	return f.CreateBlockWithReferences(p, nil, parentsCount...)
+}
+
+// CreateBlockWithReferences creates a new block with the references submit.
+func (f *Factory) CreateBlockWithReferences(p iotago.Payload, references model.ParentReferences, strongParentsCountOpt ...int) (*model.Block, error) {
+	strongParentsCount := iotago.BlockMinStrongParents
+	if len(strongParentsCountOpt) > 0 {
+		strongParentsCount = strongParentsCountOpt[0]
+	}
+
+	var err error
+	if references == nil {
+		references, err = f.tryGetReferences(p, strongParentsCount)
+		if err != nil {
+			return nil, errors.Wrap(err, "error while trying to get references")
+		}
+	}
+
+	slotCommitment, lastFinalizedSlot, err := f.commitmentFunc()
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot retrieve slot commitment")
+	}
+
+	pubKey := f.localPrivateKey.Public()
+	addr := iotago.Ed25519AddressFromPubKey(pubKey[:])
+
+	// TODO: do we have to ensure that the issuing time is at least the Max (latest) of the parents' issuing times?
+	block, err := builder.NewBlockBuilder().
+		StrongParents(references[model.StrongParentType]).
+		WeakParents(references[model.WeakParentType]).
+		ShallowLikeParents(references[model.ShallowLikeParentType]).
+		SlotCommitment(slotCommitment).
+		LatestFinalizedSlot(lastFinalizedSlot).
+		Payload(p).
+		Sign(&addr, f.localPrivateKey[:]).
+		Build()
+	if err != nil {
+		return nil, errors.Wrap(err, "error building block")
+	}
+
+	modelBlock, err := model.BlockFromBlock(block, f.api)
+	if err != nil {
+		return nil, errors.Wrap(err, "error serializing block to model block")
+	}
+
+	f.Events.BlockConstructed.Trigger(modelBlock)
+	return modelBlock, nil
+}
+
+func (f *Factory) GetReferences(p iotago.Payload, parentsCount int) (references model.ParentReferences, err error) {
+	strongParents := f.tips(p, parentsCount)
+	if len(strongParents) == 0 {
+		return nil, errors.Errorf("no strong parents were selected in tip selection")
+	}
+
+	references, err = f.referencesFunc(p, strongParents)
+	// If none of the strong parents are possible references, we have to try again.
+	if err != nil {
+		return nil, errors.Wrap(err, "references could not be created")
+	}
+
+	// TODO: should we remove duplicates in the references? It shouldn't be necessary with the new ConflictDAG.
+	return references, nil
+}
+
+func (f *Factory) tryGetReferences(p iotago.Payload, parentsCount int) (references model.ParentReferences, err error) {
+	references, err = f.GetReferences(p, parentsCount)
+	if err == nil {
+		return references, nil
+	}
+	f.Events.Error.Trigger(errors.Wrap(err, "could not get references"))
+
+	timeout := time.NewTimer(f.optsTipSelectionTimeout)
+	interval := time.NewTicker(f.optsTipSelectionRetryInterval)
+	for {
+		select {
+		case <-interval.C:
+			references, err = f.GetReferences(p, parentsCount)
+			if err != nil {
+				f.Events.Error.Trigger(errors.Wrap(err, "could not get references"))
+				continue
+			}
+
+			return references, nil
+		case <-timeout.C:
+			return nil, errors.Errorf("timeout while trying to select tips and determine references")
+		}
+	}
+}
+
+// TODO: when Ledger is refactored, we need to rework the stuff below
+func (f *Factory) tips(p iotago.Payload, parentsCount int) (parents iotago.BlockIDs) {
+	parents = f.tipManager.Tips(parentsCount)
+
+	// tx, ok := p.(utxo.Transaction)
+	// if !ok {
+	// 	return parents
+	// }
+
+	// If the block is issuing a transaction and is a double spend, we add it in parallel to the earliest attachment
+	// to prevent a double spend from being issued in its past cone.
+	// if conflictingTransactions := f.tangle.Ledger.Utils.ConflictingTransactions(tx.ID()); !conflictingTransactions.IsEmpty() {
+	//	if earliestAttachment := f.EarliestAttachment(conflictingTransactions); earliestAttachment != nil {
+	//		return earliestAttachment.ParentsByType(tangle.StrongParentType)
+	//	}
+	// }
+
+	return parents
+}
+
+// ReferencesFunc is a function type that returns like references a given set of parents of a Block.
+type ReferencesFunc func(payload iotago.Payload, strongParents iotago.BlockIDs) (references model.ParentReferences, err error)
+
+// CommitmentFunc is a function type that returns the commitment of the latest committable slot.
+type CommitmentFunc func() (commitment *iotago.Commitment, lastFinalizedSlot iotago.SlotIndex, err error)
+
+func WithTipSelectionTimeout(timeout time.Duration) options.Option[Factory] {
+	return func(factory *Factory) {
+		factory.optsTipSelectionTimeout = timeout
+	}
+}
+
+func WithTipSelectionRetryInterval(interval time.Duration) options.Option[Factory] {
+	return func(factory *Factory) {
+		factory.optsTipSelectionRetryInterval = interval
+	}
+}
---
+++ pkg/protocol/blockissuer/blockissuer.go
@@ -0,0 +1,187 @@
+package blockissuer
+
+import (
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/iotaledger/hive.go/crypto/ed25519"
+	"github.com/iotaledger/hive.go/crypto/identity"
+	"github.com/iotaledger/hive.go/runtime/event"
+	"github.com/iotaledger/hive.go/runtime/options"
+	"github.com/iotaledger/hive.go/runtime/workerpool"
+	"github.com/iotaledger/iota-core/pkg/model"
+	"github.com/iotaledger/iota-core/pkg/protocol"
+	"github.com/iotaledger/iota-core/pkg/protocol/blockissuer/blockfactory"
+	"github.com/iotaledger/iota-core/pkg/protocol/engine/blocks"
+	iotago "github.com/iotaledger/iota.go/v4"
+)
+
+// region Factory ///////////////////////////////////////////////////////////////////////////////////////////////
+
+// BlockIssuer contains logic to create and issue blocks.
+type BlockIssuer struct {
+	Events *Events
+
+	*blockfactory.Factory
+	protocol        *protocol.Protocol
+	localPrivateKey ed25519.PrivateKey
+	workerPool      *workerpool.WorkerPool
+
+	optsBlockFactoryOptions    []options.Option[blockfactory.Factory]
+	optsIgnoreBootstrappedFlag bool
+}
+
+// New creates a new block issuer.
+func New(protocol *protocol.Protocol, localPrivateKey ed25519.PrivateKey, opts ...options.Option[BlockIssuer]) *BlockIssuer {
+	return options.Apply(&BlockIssuer{
+		Events:          NewEvents(),
+		localPrivateKey: localPrivateKey,
+		protocol:        protocol,
+		workerPool:      protocol.Workers.CreatePool("BlockIssuer", 2),
+	}, opts, func(i *BlockIssuer) {
+		i.Factory = blockfactory.NewBlockFactory(
+			localPrivateKey,
+			i.protocol.TipManager,
+			func(_ iotago.Payload, strongParents iotago.BlockIDs) (model.ParentReferences, error) {
+				return model.ParentReferences{
+					model.StrongParentType: strongParents,
+				}, nil
+			},
+			func() (ecRecord *iotago.Commitment, lastFinalizedSlot iotago.SlotIndex, err error) {
+				latestCommitment := i.protocol.MainEngineInstance().Storage.Settings().LatestCommitment()
+				confirmedSlotIndex := i.protocol.MainEngineInstance().Storage.Settings().LatestFinalizedSlot()
+
+				return latestCommitment, confirmedSlotIndex, nil
+			},
+			i.optsBlockFactoryOptions...)
+	}, (*BlockIssuer).setupEvents)
+}
+
+func (i *BlockIssuer) setupEvents() {
+	i.Events.Error.LinkTo(i.Factory.Events.Error)
+}
+
+// IssuePayload creates a new block including sequence number and tip selection, submits it to be processed and returns it.
+func (i *BlockIssuer) IssuePayload(p iotago.Payload, parentsCount ...int) (block *model.Block, err error) {
+	if !i.optsIgnoreBootstrappedFlag && !i.protocol.MainEngineInstance().IsBootstrapped() {
+		return nil, ErrNotBootstraped
+	}
+
+	block, err = i.Factory.CreateBlock(p, parentsCount...)
+	if err != nil {
+		i.Events.Error.Trigger(errors.Wrap(err, "block could not be created"))
+		return block, err
+	}
+	return block, i.issueBlock(block)
+}
+
+// IssuePayloadWithReferences creates a new block with the references submit.
+func (i *BlockIssuer) IssuePayloadWithReferences(p iotago.Payload, references model.ParentReferences, strongParentsCountOpt ...int) (block *model.Block, err error) {
+	if !i.optsIgnoreBootstrappedFlag && !i.protocol.MainEngineInstance().IsBootstrapped() {
+		return nil, ErrNotBootstraped
+	}
+
+	block, err = i.Factory.CreateBlockWithReferences(p, references, strongParentsCountOpt...)
+	if err != nil {
+		i.Events.Error.Trigger(errors.Wrap(err, "block with references could not be created"))
+		return nil, err
+	}
+
+	return block, i.issueBlock(block)
+}
+
+func (i *BlockIssuer) issueBlock(block *model.Block) error {
+	if err := i.protocol.ProcessBlock(block, identity.NewID(i.localPrivateKey.Public())); err != nil {
+		return err
+	}
+	i.Events.BlockIssued.Trigger(block)
+
+	return nil
+}
+
+// IssueBlockAndAwaitBlockToBeBooked awaits maxAwait for the given block to get booked.
+func (i *BlockIssuer) IssueBlockAndAwaitBlockToBeBooked(block *model.Block, maxAwait time.Duration) error {
+	if !i.optsIgnoreBootstrappedFlag && !i.protocol.MainEngineInstance().IsBootstrapped() {
+		return ErrNotBootstraped
+	}
+
+	// first subscribe to the transaction booked event
+	booked := make(chan *blocks.Block, 1)
+	// exit is used to let the caller exit if for whatever
+	// reason the same transaction gets booked multiple times
+	exit := make(chan struct{})
+	defer close(exit)
+
+	defer i.protocol.Events.Engine.Booker.BlockBooked.Hook(func(block *blocks.Block) {
+		if block.ID() != block.ID() {
+			return
+		}
+		select {
+		case booked <- block:
+		case <-exit:
+		}
+	}, event.WithWorkerPool(i.workerPool)).Unhook()
+
+	if err := i.issueBlock(block); err != nil {
+		return errors.Wrapf(err, "failed to issue block %s", block.ID().String())
+	}
+
+	timer := time.NewTimer(maxAwait)
+	defer timer.Stop()
+	select {
+	case <-timer.C:
+		return ErrBlockWasNotBookedInTime
+	case <-booked:
+		return nil
+	}
+}
+
+// IssueBlockAndAwaitBlockToBeTracked awaits maxAwait for the given block to get tracked.
+func (i *BlockIssuer) IssueBlockAndAwaitBlockToBeTracked(block *model.Block, maxAwait time.Duration) error {
+	if !i.optsIgnoreBootstrappedFlag && !i.protocol.MainEngineInstance().IsBootstrapped() {
+		return ErrNotBootstraped
+	}
+
+	// first subscribe to the transaction booked event
+	booked := make(chan *blocks.Block, 1)
+	// exit is used to let the caller exit if for whatever
+	// reason the same transaction gets booked multiple times
+	exit := make(chan struct{})
+	defer close(exit)
+
+	defer i.protocol.Events.Engine.Booker.WitnessAdded.Hook(func(block *blocks.Block) {
+		if block.ID() != block.ID() {
+			return
+		}
+		select {
+		case booked <- block:
+		case <-exit:
+		}
+	}, event.WithWorkerPool(i.workerPool)).Unhook()
+
+	if err := i.issueBlock(block); err != nil {
+		return errors.Wrapf(err, "failed to issue block %s", block.ID().String())
+	}
+
+	timer := time.NewTimer(maxAwait)
+	defer timer.Stop()
+	select {
+	case <-timer.C:
+		return ErrBlockWasNotBookedInTime
+	case <-booked:
+		return nil
+	}
+}
+
+func WithBlockFactoryOptions(blockFactoryOptions ...options.Option[blockfactory.Factory]) options.Option[BlockIssuer] {
+	return func(issuer *BlockIssuer) {
+		issuer.optsBlockFactoryOptions = blockFactoryOptions
+	}
+}
+
+func WithIgnoreBootstrappedFlag(ignoreBootstrappedFlag bool) options.Option[BlockIssuer] {
+	return func(issuer *BlockIssuer) {
+		issuer.optsIgnoreBootstrappedFlag = ignoreBootstrappedFlag
+	}
+}
