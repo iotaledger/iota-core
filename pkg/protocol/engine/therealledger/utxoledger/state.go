7c7ff7ec1a062f8156cbf6907d64b85fb2b8a11e
Alexander Sporn <github@alexsporn.de>
2023-05-11 08:54:31 +0200

    Removed interim code

--- pkg/protocol/engine/therealledger/utxoledger/state.go
+++ pkg/protocol/engine/therealledger/utxoledger/state.go
@@ -1,20 +1,14 @@
 package utxoledger
 
 import (
-	"github.com/iotaledger/iota-core/pkg/protocol/engine/ledgerstate"
 	iotago "github.com/iotaledger/iota.go/v4"
 )
 
 type State struct {
 	outputID iotago.OutputID
 	output   iotago.Output
-
-	utxoOutput *ledgerstate.Output
 }
 
 func (s *State) ID() iotago.OutputID {
-	if s.utxoOutput != nil {
-		return s.utxoOutput.OutputID()
-	}
 	return s.outputID
 }
