530836ec05f2dddbf46dee57acf8d9d9da54b195
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2023-05-22 16:18:34 +0200

    Implement a transaction framework for easy testing.

--- pkg/tests/booker_test.go
+++ pkg/tests/booker_test.go
@@ -6,16 +6,13 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/iotaledger/hive.go/lo"
 	"github.com/iotaledger/hive.go/runtime/options"
 	"github.com/iotaledger/iota-core/pkg/blockissuer"
 	"github.com/iotaledger/iota-core/pkg/protocol"
+	"github.com/iotaledger/iota-core/pkg/protocol/engine/blocks"
 	"github.com/iotaledger/iota-core/pkg/protocol/engine/notarization/slotnotarization"
 	"github.com/iotaledger/iota-core/pkg/protocol/snapshotcreator"
 	"github.com/iotaledger/iota-core/pkg/testsuite"
-	"github.com/iotaledger/iota-core/pkg/testsuite/mock"
-	iotago "github.com/iotaledger/iota.go/v4"
-	"github.com/iotaledger/iota.go/v4/builder"
 	"github.com/iotaledger/iota.go/v4/tpkg"
 )
 
@@ -25,9 +22,6 @@ func TestBooker(t *testing.T) {
 		snapshotcreator.WithGenesisSeed(genesisSeed[:]),
 	))
 	defer ts.Shutdown()
-
-	walletFrom := mock.NewHDWallet("genesis", genesisSeed[:], 0)
-	walletTo := mock.NewHDWallet("genesis", genesisSeed[:], 2)
 
 	node1 := ts.AddValidatorNode("node1", 1)
 	ts.Run(map[string][]options.Option[protocol.Protocol]{
@@ -39,30 +33,24 @@ func TestBooker(t *testing.T) {
 	})
 	time.Sleep(time.Second)
 
-	output, err := node1.Protocol.MainEngineInstance().Ledger.Output(&iotago.UTXOInput{TransactionID: iotago.TransactionID{}, TransactionOutputIndex: 0})
+	transactionFramework := testsuite.NewTransactionFramework(node1.Protocol, genesisSeed[:])
+	tx1, err := transactionFramework.CreateTransaction("Tx1", 1, "Genesis")
 	require.NoError(t, err)
 
-	transaction, err := builder.NewTransactionBuilder(ts.Node("node1").Protocol.MainEngineInstance().Storage.Settings().ProtocolParameters().NetworkID()).
-		AddInput(&builder.TxInput{UnlockTarget: output.Output().UnlockConditionSet().Address().Address, InputID: output.OutputID(), Input: output.Output()}).
-		AddOutput(&iotago.BasicOutput{
-			Amount: 10000000,
-			Conditions: iotago.BasicOutputUnlockConditions{
-				&iotago.AddressUnlockCondition{Address: walletTo.Address()},
-			},
-		}).
-		Build(node1.Protocol.MainEngineInstance().Storage.Settings().ProtocolParameters(), iotago.NewInMemoryAddressSigner(iotago.NewAddressKeysForEd25519Address(walletFrom.Address(), lo.Return1(walletFrom.KeyPair()))))
-	require.NoError(t, err)
+	tx2, err := transactionFramework.CreateTransaction("Tx2", 1, "Tx1:0")
 
-	node1.IssueBlock("block1", blockissuer.WithPayload(transaction))
+	blocksBooked := 0
+	node1.Protocol.MainEngineInstance().Events.Booker.BlockBooked.Hook(func(_ *blocks.Block) {
+		blocksBooked++
+	})
 
+	node1.IssueBlock("block1", blockissuer.WithPayload(tx2))
 	ts.Wait(node1)
 
-	newOutput, err := node1.Protocol.MainEngineInstance().Ledger.Output(&iotago.UTXOInput{TransactionID: lo.PanicOnErr(transaction.ID()), TransactionOutputIndex: 0})
-	require.NoError(t, err)
+	require.Equal(t, 0, blocksBooked)
 
-	genesisOutput, err := node1.Protocol.MainEngineInstance().Ledger.Output(&iotago.UTXOInput{TransactionID: iotago.TransactionID{}, TransactionOutputIndex: 0})
-	require.NoError(t, err)
+	node1.IssueBlock("block2", blockissuer.WithPayload(tx1))
+	ts.Wait(node1)
 
-	require.EqualValues(t, 0, genesisOutput.SlotIndexBooked())
-	require.EqualValues(t, 101, newOutput.SlotIndexBooked())
+	require.Equal(t, 2, blocksBooked)
 }
