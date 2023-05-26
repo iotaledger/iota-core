06fd0dc34a8ebac6f3c80cdd2850d32dd3969a5a
jkrvivian <jkrvivian@gmail.com>
2023-05-26 10:05:51 +0800

    Make Commitment optional in OutputMetadata API

--- components/coreapi/utxo.go
+++ components/coreapi/utxo.go
@@ -1,8 +1,6 @@
 package coreapi
 
 import (
-	"errors"
-
 	"github.com/labstack/echo/v4"
 
 	"github.com/iotaledger/inx-app/pkg/httpserver"
@@ -11,8 +9,6 @@ import (
 	restapipkg "github.com/iotaledger/iota-core/pkg/restapi"
 	iotago "github.com/iotaledger/iota.go/v4"
 )
-
-var ErrOutputNotCommitted = errors.New("the included slot index of the requested output is not committed yet")
 
 func getOutput(c echo.Context) (*ledgerstate.Output, error) {
 	outputID, err := httpserver.ParseOutputIDParam(c, restapipkg.ParameterOutputID)
@@ -54,24 +50,24 @@ func newOutputMetadataResponse(outputID iotago.OutputID, latestCommitment *model
 		return nil, err
 	}
 
+	resp := &outputMetadataResponse{
+		BlockID:            output.BlockID().ToHex(),
+		TransactionID:      outputID.TransactionID().ToHex(),
+		OutputIndex:        outputID.Index(),
+		IsSpent:            false,
+		LatestCommitmentID: latestCommitment.ID().ToHex(),
+	}
+
 	includedSlotIndex := output.SlotIndexBooked()
-	if includedSlotIndex > latestCommitment.Index() {
-		return nil, ErrOutputNotCommitted
+	if includedSlotIndex <= latestCommitment.Index() {
+		includedCommitment, err := deps.Protocol.MainEngineInstance().Storage.Permanent.Commitments().Load(includedSlotIndex)
+		if err != nil {
+			return nil, err
+		}
+		resp.IncludedCommitmentID = includedCommitment.ID().ToHex()
 	}
 
-	includedCommitment, err := deps.Protocol.MainEngineInstance().Storage.Permanent.Commitments().Load(includedSlotIndex)
-	if err != nil {
-		return nil, err
-	}
-
-	return &outputMetadataResponse{
-		BlockID:              output.BlockID().ToHex(),
-		TransactionID:        outputID.TransactionID().ToHex(),
-		OutputIndex:          outputID.Index(),
-		IsSpent:              false,
-		IncludedCommitmentID: includedCommitment.ID().ToHex(),
-		LatestCommitmentID:   latestCommitment.ID().ToHex(),
-	}, nil
+	return resp, nil
 }
 
 func newSpentMetadataResponse(outputID iotago.OutputID, latestCommitment *model.Commitment) (*outputMetadataResponse, error) {
@@ -80,31 +76,32 @@ func newSpentMetadataResponse(outputID iotago.OutputID, latestCommitment *model.
 		return nil, err
 	}
 
+	resp := &outputMetadataResponse{
+		BlockID:            ledgerOutput.BlockID().ToHex(),
+		TransactionID:      outputID.TransactionID().ToHex(),
+		OutputIndex:        outputID.Index(),
+		IsSpent:            true,
+		TransactionIDSpent: ledgerOutput.TransactionIDSpent().ToHex(),
+		LatestCommitmentID: latestCommitment.ID().ToHex(),
+	}
+
 	includedSlotIndex := ledgerOutput.Output().SlotIndexBooked()
+	if includedSlotIndex <= latestCommitment.Index() {
+		includedCommitment, err := deps.Protocol.MainEngineInstance().Storage.Permanent.Commitments().Load(includedSlotIndex)
+		if err != nil {
+			return nil, err
+		}
+		resp.IncludedCommitmentID = includedCommitment.ID().ToHex()
+	}
+
 	spentSlotIndex := ledgerOutput.SlotIndexSpent()
-
-	if includedSlotIndex > latestCommitment.Index() || spentSlotIndex > latestCommitment.Index() {
-		return nil, ErrOutputNotCommitted
+	if spentSlotIndex <= latestCommitment.Index() {
+		spentCommitment, err := deps.Protocol.MainEngineInstance().Storage.Permanent.Commitments().Load(spentSlotIndex)
+		if err != nil {
+			return nil, err
+		}
+		resp.CommitmentIDSpent = spentCommitment.ID().ToHex()
 	}
 
-	includedCommitment, err := deps.Protocol.MainEngineInstance().Storage.Permanent.Commitments().Load(includedSlotIndex)
-	if err != nil {
-		return nil, err
-	}
-
-	spentCommitment, err := deps.Protocol.MainEngineInstance().Storage.Permanent.Commitments().Load(spentSlotIndex)
-	if err != nil {
-		return nil, err
-	}
-
-	return &outputMetadataResponse{
-		BlockID:              ledgerOutput.BlockID().ToHex(),
-		TransactionID:        outputID.TransactionID().ToHex(),
-		OutputIndex:          outputID.Index(),
-		IsSpent:              true,
-		TransactionIDSpent:   ledgerOutput.TransactionIDSpent().ToHex(),
-		CommitmentIDSpent:    spentCommitment.ID().ToHex(),
-		IncludedCommitmentID: includedCommitment.ID().ToHex(),
-		LatestCommitmentID:   latestCommitment.ID().ToHex(),
-	}, nil
+	return resp, nil
 }
