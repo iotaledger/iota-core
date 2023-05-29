2515987022255faec65fc1545137e9fb19a77bc8
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-05-30 00:12:41 +0200

    Refactor: added missing comments

--- pkg/protocol/tipmanager/events.go
+++ pkg/protocol/tipmanager/events.go
@@ -2,12 +2,16 @@ package tipmanager
 
 import "github.com/iotaledger/hive.go/runtime/event"
 
+// Events represents events happening in the TipManager.
 type Events struct {
+	// BlockAdded gets triggered when a new block was added to the TipManager.
 	BlockAdded *event.Event1[TipMetadata]
 
+	// Group makes the Events linkable through the central Events dictionary.
 	event.Group[Events, *Events]
 }
 
+// NewEvents creates a new Events instance.
 var NewEvents = event.CreateGroupConstructor(func() *Events {
 	return &Events{
 		BlockAdded: event.New1[TipMetadata](),
