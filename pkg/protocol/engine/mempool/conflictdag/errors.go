39bd518795346d5319a97ea4c60dc0db5818eba3
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2023-05-30 13:47:56 +0200

    Add wrongly removed error

--- pkg/protocol/engine/mempool/conflictdag/errors.go
+++ pkg/protocol/engine/mempool/conflictdag/errors.go
@@ -3,6 +3,7 @@ package conflictdag
 import "golang.org/x/xerrors"
 
 var (
+	ErrExpected                 = xerrors.New("expected error")
 	ErrAlreadyPartOfConflictSet = xerrors.New("conflict already part of ConflictSet")
 	ErrEntityEvicted            = xerrors.New("tried to operate on evicted entity")
 	ErrFatal                    = xerrors.New("fatal error")
