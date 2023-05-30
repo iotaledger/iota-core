1bbc6d9deafc0eb345bcbd0c51b12549e7d24442
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-05-30 13:57:59 +0800

    Fix double locking when obtaining rootcommitment

--- pkg/protocol/chainmanager/manager.go
+++ pkg/protocol/chainmanager/manager.go
@@ -328,7 +328,7 @@ func (m *Manager) forkingPointAgainstMainChain(commitment *ChainCommitment) (*Ch
 
 	var forkingCommitment *ChainCommitment
 	// Walk all possible forks until we reach our main chain by jumping over each forking point
-	for chain := commitment.Chain(); chain != m.RootCommitment().Chain(); chain = commitment.Chain() {
+	for chain := commitment.Chain(); chain != m.rootCommitment.Chain(); chain = commitment.Chain() {
 		forkingCommitment = chain.ForkingPoint
 
 		if commitment, _ = m.commitment(forkingCommitment.Commitment().PrevID()); commitment == nil {
