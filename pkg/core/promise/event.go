cb471151e58275b1ffd050d9c778e258a6b85e4b
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-06-18 21:57:46 +0200

    Feat: cleaned up promise events

--- pkg/core/promise/event.go
+++ pkg/core/promise/event.go
@@ -4,20 +4,26 @@ import (
 	"sync"
 )
 
+// region Event
+
+// Event is an event that can be triggered exactly once. Consumers that register themselves after the event was
+// triggered, will be called immediately.
 type Event struct {
-	// callbacks is nil if the event was already triggered.
+	// callbacks is a slice of callbacks that will be called when the event is triggered.
 	callbacks []func()
 
-	// mutex is used to synchronize access to the callbacks slice.
+	// mutex is used to synchronize access to the callbacks.
 	mutex sync.RWMutex
 }
 
+// NewEvent creates a new Event with 0 generic parameters.
 func NewEvent() *Event {
 	return &Event{
 		callbacks: make([]func(), 0),
 	}
 }
 
+// Trigger triggers the event. If the event was already triggered, this method does nothing.
 func (f *Event) Trigger() (wasTriggered bool) {
 	for _, callback := range func() (callbacks []func()) {
 		f.mutex.Lock()
@@ -36,23 +42,26 @@ func (f *Event) Trigger() (wasTriggered bool) {
 	return
 }
 
-func (f *Event) queueCallback(callback func()) (callbackQueued bool) {
-	f.mutex.Lock()
-	defer f.mutex.Unlock()
-
-	if callbackQueued = f.callbacks != nil; callbackQueued {
-		f.callbacks = append(f.callbacks, callback)
-	}
-
-	return callbackQueued
-}
-
+// OnTrigger registers a callback that will be called when the event is triggered. If the event was already triggered,
+// the callback will be called immediately.
 func (f *Event) OnTrigger(callback func()) {
-	if !f.queueCallback(callback) {
+	if !func() (callbackRegistered bool) {
+		f.mutex.Lock()
+		defer f.mutex.Unlock()
+
+		if f.callbacks == nil {
+			return false
+		}
+
+		f.callbacks = append(f.callbacks, callback)
+
+		return true
+	}() {
 		callback()
 	}
 }
 
+// WasTriggered returns true if the event was already triggered.
 func (f *Event) WasTriggered() bool {
 	f.mutex.RLock()
 	defer f.mutex.RUnlock()
@@ -60,27 +69,39 @@ func (f *Event) WasTriggered() bool {
 	return f.callbacks == nil
 }
 
-type Event1[T any] struct {
-	triggeredCallbacks []func(T)
-	triggerResult      *T
+// endregion
 
+// region Event1
+
+// Event1 is an event with a single parameter that can be triggered exactly once. Consumers that register themselves
+// after the event was triggered, will be called immediately.
+type Event1[T any] struct {
+	// callbacks is a slice of callbacks that will be called when the event is triggered.
+	callbacks []func(T)
+
+	// value is the value that was passed to the Trigger method.
+	value *T
+
+	// mutex is used to synchronize access to the callbacks.
 	mutex sync.RWMutex
 }
 
+// NewEvent1 creates a new event with 1 generic parameter.
 func NewEvent1[T any]() *Event1[T] {
 	return &Event1[T]{
-		triggeredCallbacks: make([]func(T), 0),
+		callbacks: make([]func(T), 0),
 	}
 }
 
+// Trigger triggers the event. If the event was already triggered, this method does nothing.
 func (f *Event1[T]) Trigger(arg T) {
 	for _, callback := range func() (callbacks []func(T)) {
 		f.mutex.Lock()
 		defer f.mutex.Unlock()
 
-		if callbacks = f.triggeredCallbacks; callbacks != nil {
-			f.triggeredCallbacks = nil
-			f.triggerResult = &arg
+		if callbacks = f.callbacks; callbacks != nil {
+			f.callbacks = nil
+			f.value = &arg
 		}
 
 		return callbacks
@@ -89,20 +110,31 @@ func (f *Event1[T]) Trigger(arg T) {
 	}
 }
 
+// OnTrigger registers a callback that will be called when the event is triggered. If the event was already triggered,
+// the callback will be called immediately.
 func (f *Event1[T]) OnTrigger(callback func(T)) {
-	f.mutex.Lock()
-	defer f.mutex.Unlock()
+	if !func() (callbackRegistered bool) {
+		f.mutex.Lock()
+		defer f.mutex.Unlock()
 
-	if f.triggeredCallbacks != nil {
-		f.triggeredCallbacks = append(f.triggeredCallbacks, callback)
-	} else {
-		callback(*f.triggerResult)
+		if f.callbacks == nil {
+			return false
+		}
+
+		f.callbacks = append(f.callbacks, callback)
+
+		return true
+	}() {
+		callback(*f.value)
 	}
 }
 
+// WasTriggered returns true if the event was already triggered.
 func (f *Event1[T]) WasTriggered() bool {
 	f.mutex.RLock()
 	defer f.mutex.RUnlock()
 
-	return f.triggeredCallbacks == nil
+	return f.callbacks == nil
 }
+
+// endregion
