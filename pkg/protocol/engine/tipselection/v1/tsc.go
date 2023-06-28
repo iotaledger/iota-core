136b1405925a853912b806b78f4111cecc31d549
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-06-28 02:50:17 +0200

    Feat: simplified TSC Manager

--- pkg/protocol/engine/tipselection/v1/tsc.go
+++ pkg/protocol/engine/tipselection/v1/tsc.go
@@ -3,51 +3,48 @@ package tipselectionv1
 import (
 	"time"
 
+	"github.com/iotaledger/hive.go/lo"
 	lpromise "github.com/iotaledger/iota-core/pkg/core/promise"
 	"github.com/iotaledger/iota-core/pkg/core/timed"
 	"github.com/iotaledger/iota-core/pkg/protocol/engine/tipmanager"
 )
 
 type TSCManager struct {
-	tscQueue     timed.PriorityQueue[tipmanager.TipMetadata]
-	tscThreshold *lpromise.Value[time.Time]
+	queue                     timed.PriorityQueue[tipmanager.TipMetadata]
+	blockIssuingTimeThreshold *lpromise.Value[time.Time]
 }
 
 func NewTSCManager() *TSCManager {
 	t := &TSCManager{
-		tscQueue:     timed.NewPriorityQueue[tipmanager.TipMetadata](true),
-		tscThreshold: lpromise.NewValue[time.Time](),
+		queue:                     timed.NewPriorityQueue[tipmanager.TipMetadata](true),
+		blockIssuingTimeThreshold: lpromise.NewValue[time.Time](),
 	}
 
-	t.tscThreshold.OnUpdate(func(_, newThreshold time.Time) {
-		t.markTipsPastThreshold(newThreshold)
+	t.blockIssuingTimeThreshold.OnUpdate(func(_, newThreshold time.Time) {
+		t.markTipsBlockIssuingTimeThresholdReached(newThreshold)
 	})
 
 	return t
 }
 
 func (t *TSCManager) Add(tip tipmanager.TipMetadata) {
-	t.tscThreshold.Read(func(threshold time.Time) {
+	t.blockIssuingTimeThreshold.Read(func(threshold time.Time) {
 		if threshold.After(tip.Block().IssuingTime()) {
-			// TODO: tip.SetTSCThresholdReached()
+			// TODO: tip.SetBlockIssuingTimeThresholdReached()
 		} else {
-			t.tscQueue.Push(tip, tip.Block().IssuingTime())
+			t.queue.Push(tip, tip.Block().IssuingTime())
 		}
 	})
 }
 
-func (t *TSCManager) UpdateThreshold(newThreshold time.Time) {
-	t.tscThreshold.Compute(func(currentThreshold time.Time) time.Time {
-		if newThreshold.Before(currentThreshold) {
-			return currentThreshold
-		}
-
-		return newThreshold
+func (t *TSCManager) UpdateBlockIssuingTimeThreshold(newThreshold time.Time) {
+	t.blockIssuingTimeThreshold.Compute(func(currentThreshold time.Time) time.Time {
+		return lo.Cond(newThreshold.Before(currentThreshold), currentThreshold, newThreshold)
 	})
 }
 
-func (t *TSCManager) markTipsPastThreshold(threshold time.Time) {
-	for _, _ = range t.tscQueue.PopUntil(threshold) {
-		// TODO: tip.SetTSCThresholdReached()
+func (t *TSCManager) markTipsBlockIssuingTimeThresholdReached(threshold time.Time) {
+	for _, _ = range t.queue.PopUntil(threshold) {
+		// TODO: tip.SetBlockIssuingTimeThresholdReached()
 	}
 }
