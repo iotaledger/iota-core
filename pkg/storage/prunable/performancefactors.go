1faa6929d3ffdbf4c0f5258927bd3b9833e34eee
muXxer <git@muxxer.de>
2023-07-04 16:29:04 +0200

    Fix iteration in ForEachPerformanceFactor

--- pkg/storage/prunable/performancefactors.go
+++ pkg/storage/prunable/performancefactors.go
@@ -46,23 +46,25 @@ func (p *PerformanceFactors) Store(accountID iotago.AccountID, pf uint64) error 
 	return p.store.Set(lo.PanicOnErr(accountID.Bytes()), lo.PanicOnErr(storable.SerializableInt64(pf).Bytes()))
 }
 
-// ForEachPerformanceFactor iterates over all saved validators. Note that this stream function won't call the consumer function for inactive validators, so better to use the load over
-// every member of the committee if needed.
+// ForEachPerformanceFactor iterates over all saved validators.
+// Note that this stream function won't call the consumer function for inactive validators,
+// so better to use the load over every member of the committee if needed.
 func (p *PerformanceFactors) ForEachPerformanceFactor(consumer func(accountID iotago.AccountID, pf uint64) error) error {
 	var innerErr error
 	if err := p.store.Iterate(kvstore.EmptyPrefix, func(key kvstore.Key, value kvstore.Value) bool {
 		var accountID iotago.AccountID
-		if _, innerErr = accountID.FromBytes(key); innerErr != nil {
+		if _, err := accountID.FromBytes(key); err != nil {
+			innerErr = err
 			return false
 		}
 
 		serializablePf := new(storable.SerializableInt64)
-		_, err := serializablePf.FromBytes(value)
-		if err != nil {
+		if _, err := serializablePf.FromBytes(value); err != nil {
+			innerErr = err
 			return false
 		}
 
-		return consumer(accountID, uint64(*serializablePf)) != nil
+		return consumer(accountID, uint64(*serializablePf)) == nil
 	}); err != nil {
 		return errors.Wrapf(err, "failed to stream performance factors for slot %s", p.slot)
 	}
