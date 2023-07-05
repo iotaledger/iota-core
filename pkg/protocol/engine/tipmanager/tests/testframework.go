b4800a50c6f7430d710db56548be4c17edfc7484
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-07-05 15:01:05 +0200

    Fix: fixed merge error

--- pkg/protocol/engine/tipmanager/tests/testframework.go
+++ pkg/protocol/engine/tipmanager/tests/testframework.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/iotaledger/hive.go/ds/advancedset"
 	"github.com/iotaledger/hive.go/lo"
-	"github.com/iotaledger/iota-core/pkg/core/account"
 	"github.com/iotaledger/iota-core/pkg/model"
 	"github.com/iotaledger/iota-core/pkg/protocol/engine/blocks"
 	"github.com/iotaledger/iota-core/pkg/protocol/engine/tipmanager"
