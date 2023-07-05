2029d0ac8f2f401af1ffaf75479f054f0e489e2f
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-07-05 02:51:50 +0200

    Feat: made naming consistent

--- pkg/core/agential/derived_value_receptor.go
+++ pkg/core/agential/derived_value_receptor.go
@@ -31,7 +31,7 @@ func (d *DerivedValueReceptor[ValueType]) Unsubscribe() {
 }
 
 // DeriveValueReceptorFrom1Input creates a DerivedValueReceptor that transforms the value of one input value receptor into a different one.
-func DeriveValueReceptorFrom1Input[ValueType, InputType1 comparable, InputSource1 Value[InputType1]](containingAgent Agent, transformationFunc func(InputType1) ValueType, input1 *InputSource1) *DerivedValueReceptor[ValueType] {
+func DeriveValueReceptorFrom1Input[ValueType, InputType1 comparable, InputValueType1 Value[InputType1]](containingAgent Agent, transformationFunc func(InputType1) ValueType, input1 *InputValueType1) *DerivedValueReceptor[ValueType] {
 	d := &DerivedValueReceptor[ValueType]{
 		ValueReceptor: NewValueReceptor[ValueType](),
 	}
@@ -53,7 +53,7 @@ func DeriveValueReceptorFrom1Input[ValueType, InputType1 comparable, InputSource
 }
 
 // DeriveValueReceptorFrom2Inputs creates a DerivedValueReceptor that transforms the value of two input value receptors into a different one.
-func DeriveValueReceptorFrom2Inputs[OutputType, InputType1, InputType2 comparable, InputSource1 Value[InputType1], InputSource2 Value[InputType2]](containingAgent Agent, transitionFunc func(InputType1, InputType2) OutputType, input1 *InputSource1, input2 *InputSource2) *DerivedValueReceptor[OutputType] {
+func DeriveValueReceptorFrom2Inputs[OutputType, InputType1, InputType2 comparable, InputValueType1 Value[InputType1], InputValueType2 Value[InputType2]](containingAgent Agent, transitionFunc func(InputType1, InputType2) OutputType, input1 *InputValueType1, input2 *InputValueType2) *DerivedValueReceptor[OutputType] {
 	d := &DerivedValueReceptor[OutputType]{
 		ValueReceptor: NewValueReceptor[OutputType](),
 	}
@@ -81,7 +81,7 @@ func DeriveValueReceptorFrom2Inputs[OutputType, InputType1, InputType2 comparabl
 }
 
 // DeriveValueFrom3Inputs creates a DerivedValueReceptor that transforms the value of three input ValueReceptors into a different one.
-func DeriveValueFrom3Inputs[OutputType, InputType1, InputType2, InputType3 comparable, InputReceptorType1 Value[InputType1], InputReceptorType2 Value[InputType2], InputReceptorType3 Value[InputType3]](containingAgent Agent, transitionFunc func(InputType1, InputType2, InputType3) OutputType, input1 *InputReceptorType1, input2 *InputReceptorType2, input3 *InputReceptorType3) *DerivedValueReceptor[OutputType] {
+func DeriveValueFrom3Inputs[OutputType, InputType1, InputType2, InputType3 comparable, InputValueType1 Value[InputType1], InputValueType2 Value[InputType2], InputValueType3 Value[InputType3]](containingAgent Agent, transitionFunc func(InputType1, InputType2, InputType3) OutputType, input1 *InputValueType1, input2 *InputValueType2, input3 *InputValueType3) *DerivedValueReceptor[OutputType] {
 	d := &DerivedValueReceptor[OutputType]{
 		ValueReceptor: NewValueReceptor[OutputType](),
 	}
