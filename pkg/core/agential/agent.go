89405943945430769b7f630f2230ab7bf6b33575
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-07-05 02:47:04 +0200

    Fix: fixed minor issues

--- pkg/core/agential/agent.go
+++ pkg/core/agential/agent.go
@@ -3,6 +3,6 @@ package agential
 // Agent is the interface that describes the minimal functionality of an agent as an entity that is separated from its
 // environment.
 type Agent interface {
-	// Constructed returns a ValueReceptor that is used to signal that the agent has been constructed.
+	// Constructed returns a Value that is used to indicate when the agent has instantiated all of its components.
 	Constructed() Value[bool]
 }
--- pkg/core/agential/callback.go
+++ pkg/core/agential/callback.go
@@ -8,12 +8,12 @@ import (
 
 // callback is an internal wrapper for a callback function that is extended by an ID and a mutex (for call order
 // synchronization).
-type callback[T any] struct {
+type callback[FuncType any] struct {
 	// ID is the unique identifier of the callback.
 	ID types.UniqueID
 
 	// Invoke is the callback function that is invoked when the callback is triggered.
-	Invoke T
+	Invoke FuncType
 
 	// unsubscribed is a flag that indicates whether the callback was unsubscribed.
 	unsubscribed bool
@@ -26,15 +26,15 @@ type callback[T any] struct {
 }
 
 // newCallback is the constructor for the callback type.
-func newCallback[T any](id types.UniqueID, invoke T) *callback[T] {
-	return &callback[T]{
+func newCallback[FuncType any](id types.UniqueID, invoke FuncType) *callback[FuncType] {
+	return &callback[FuncType]{
 		ID:     id,
 		Invoke: invoke,
 	}
 }
 
 // Lock locks the callback for the given update and returns true if the callback was locked successfully.
-func (c *callback[T]) Lock(updateID types.UniqueID) bool {
+func (c *callback[FuncType]) Lock(updateID types.UniqueID) bool {
 	c.mutex.Lock()
 
 	if c.unsubscribed || updateID != 0 && updateID == c.lastUpdate {
@@ -49,12 +49,12 @@ func (c *callback[T]) Lock(updateID types.UniqueID) bool {
 }
 
 // Unlock unlocks the callback.
-func (c *callback[T]) Unlock() {
+func (c *callback[FuncType]) Unlock() {
 	c.mutex.Unlock()
 }
 
 // MarkUnsubscribed marks the callback as unsubscribed (it will no longer trigger).
-func (c *callback[T]) MarkUnsubscribed() {
+func (c *callback[FuncType]) MarkUnsubscribed() {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 
