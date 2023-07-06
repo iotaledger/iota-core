848a1218bd5837d3d869ba82c8986f68c2e41964
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-07-07 01:26:06 +0200

    Fix: used getter

--- pkg/core/reactive/set_test.go
+++ pkg/core/reactive/set_test.go
@@ -18,18 +18,18 @@ func TestSet(t *testing.T) {
 	source1.Add(advancedset.New(1, 2, 4))
 	source2.Add(advancedset.New(7, 9))
 
-	require.True(t, inheritedSet.Has(1))
-	require.True(t, inheritedSet.Has(2))
-	require.True(t, inheritedSet.Has(4))
-	require.True(t, inheritedSet.Has(7))
-	require.True(t, inheritedSet.Has(9))
+	require.True(t, inheritedSet.Get().Has(1))
+	require.True(t, inheritedSet.Get().Has(2))
+	require.True(t, inheritedSet.Get().Has(4))
+	require.True(t, inheritedSet.Get().Has(7))
+	require.True(t, inheritedSet.Get().Has(9))
 
 	inheritedSet1 := NewSet[int]()
 	inheritedSet1.InheritFrom(source1, source2)
 
-	require.True(t, inheritedSet1.Has(1))
-	require.True(t, inheritedSet1.Has(2))
-	require.True(t, inheritedSet1.Has(4))
-	require.True(t, inheritedSet1.Has(7))
-	require.True(t, inheritedSet1.Has(9))
+	require.True(t, inheritedSet1.Get().Has(1))
+	require.True(t, inheritedSet1.Get().Has(2))
+	require.True(t, inheritedSet1.Get().Has(4))
+	require.True(t, inheritedSet1.Get().Has(7))
+	require.True(t, inheritedSet1.Get().Has(9))
 }
