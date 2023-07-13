90aa881b8620ff4ac947ea447a78703a7425d3a3
Alexander Sporn <github@alexsporn.de>
2023-07-13 11:32:25 +0200

    Calm the doggo

--- pkg/protocol/engine/ledger/ledger/ledger.go
+++ pkg/protocol/engine/ledger/ledger/ledger.go
@@ -259,6 +259,7 @@ func (l *Ledger) OutputOrSpent(outputID iotago.OutputID) (*utxoledger.Output, *u
 	if !unspent {
 		spent, err := l.utxoLedger.ReadSpentForOutputIDWithoutLocking(outputID)
 		l.utxoLedger.ReadUnlockLedger()
+
 		return nil, spent, err
 	}
 
