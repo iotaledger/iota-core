ea617c51d884b0fb8e3c729256a4e1dc8ac83985
Andrea V <1577639+karimodm@users.noreply.github.com>
2023-07-13 12:18:34 +0200

    Debug models

--- components/debugapi/debug_models.go
+++ components/debugapi/debug_models.go
@@ -1,8 +1,29 @@
 package debugapi
 
+import iotago "github.com/iotaledger/iota.go/v4"
+
 type (
 	ValidatorsSummaryResponse struct {
 		ValidatorSeats map[int]string `json:"validatorSeats"`
 		ActiveSeats    []int          `json:"activeSeats"`
 	}
+
+	BlockChangesResponse struct {
+		// The index of the requested commitment.
+		Index iotago.SlotIndex `json:"index"`
+		// The blocks that got included in this slot.
+		IncludedBlocks []string `json:"includedBlocks"`
+		// The tangle root of the slot.
+		TangleRoot string `json:"tangleRoot"`
+	}
+
+	TransactionsChangesResponse struct {
+		// The index of the requested commitment.
+		Index iotago.SlotIndex `json:"index"`
+		// The transactions that got included in this slot.
+		IncludedTransactions []string `json:"includedTransactions"`
+		// The mutations root of the slot.
+		MutationsRoot string `json:"mutationsRoot"`
+	}
+
 )
