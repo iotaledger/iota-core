a5373cdc0b83818b21914ab258fa224d5dfee6bc
Alexander Sporn <github@alexsporn.de>
2023-07-14 09:33:21 +0200

    Added commitments read and listen to inx

---
+++ components/inx/server_commitments.go
@@ -0,0 +1,74 @@
+package inx
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/iotaledger/hive.go/ierrors"
+	"github.com/iotaledger/hive.go/kvstore"
+	"github.com/iotaledger/hive.go/runtime/event"
+	"github.com/iotaledger/hive.go/runtime/workerpool"
+	inx "github.com/iotaledger/inx/go"
+	"github.com/iotaledger/iota-core/pkg/model"
+	"github.com/iotaledger/iota-core/pkg/protocol/engine/notarization"
+	iotago "github.com/iotaledger/iota.go/v4"
+)
+
+func inxCommitment(commitment *model.Commitment) *inx.Commitment {
+	return &inx.Commitment{
+		CommitmentId: inx.NewCommitmentId(commitment.ID()),
+		Commitment: &inx.RawCommitment{
+			Data: commitment.Data(),
+		},
+	}
+}
+func (s *Server) ReadCommitment(_ context.Context, req *inx.CommitmentRequest) (*inx.Commitment, error) {
+	commitmentIndex := iotago.SlotIndex(req.GetCommitmentIndex())
+
+	if req.GetCommitmentId() != nil {
+		commitmentIndex = req.GetCommitmentId().Unwrap().Index()
+	}
+
+	commitment, err := deps.Protocol.MainEngineInstance().Storage.Commitments().Load(commitmentIndex)
+	if err != nil {
+		if ierrors.Is(err, kvstore.ErrKeyNotFound) {
+			return nil, status.Errorf(codes.NotFound, "commitment index %d not found", req.GetCommitmentIndex())
+		}
+		return nil, err
+	}
+
+	if req.GetCommitmentId() != nil {
+		// If it was requested by id, make sure the id matches the commitment.
+		if commitment.ID() != req.GetCommitmentId().Unwrap() {
+			return nil, status.Errorf(codes.NotFound, "commitment id %s not found, found %s instead", req.GetCommitmentId().Unwrap(), commitment.ID())
+		}
+	}
+
+	return inxCommitment(commitment), nil
+}
+
+func (s *Server) ListenToCommitments(_ *inx.NoParams, srv inx.INX_ListenToCommitmentsServer) error {
+	ctx, cancel := context.WithCancel(Component.Daemon().ContextStopped())
+
+	wp := workerpool.New("ListenToLatestCommitments", workerCount).Start()
+	unhook := deps.Protocol.Events.Engine.Notarization.SlotCommitted.Hook(func(details *notarization.SlotCommittedDetails) {
+		payload := inxCommitment(details.Commitment)
+		if err := srv.Send(payload); err != nil {
+			Component.LogErrorf("send error: %v", err)
+			cancel()
+		}
+	}, event.WithWorkerPool(wp)).Unhook
+
+	<-ctx.Done()
+	unhook()
+
+	// We need to wait until all tasks are done, otherwise we might call
+	// "SendMsg" and "CloseSend" in parallel on the grpc stream, which is
+	// not safe according to the grpc docs.
+	wp.Shutdown()
+	wp.ShutdownComplete.Wait()
+
+	return ctx.Err()
+}
