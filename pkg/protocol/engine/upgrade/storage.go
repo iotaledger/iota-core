223e845648ea36c0c2b0a830444696369c463cfb
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-07-17 11:27:48 +0200

    Implement startup from disk

---
+++ pkg/protocol/engine/upgrade/storage.go
@@ -0,0 +1,28 @@
+package upgrade
+
+import (
+	"github.com/iotaledger/hive.go/ierrors"
+	"github.com/iotaledger/iota-core/pkg/core/account"
+	"github.com/iotaledger/iota-core/pkg/storage/prunable"
+	iotago "github.com/iotaledger/iota.go/v4"
+)
+
+func (o *Orchestrator) RestoreFromDisk(slot iotago.SlotIndex) error {
+	o.evictionMutex.Lock()
+	defer o.evictionMutex.Unlock()
+
+	o.lastCommittedSlot = slot
+
+	// Load latest signals into cache into the next slot (necessary so that we have the correct information when we commit that slot).
+	latestSignals := o.latestSignals.Get(slot+1, true)
+	upgradeSignals := o.upgradeSignalsFunc(slot)
+	if err := upgradeSignals.Stream(func(seat account.SeatIndex, signaledBlock *prunable.SignaledBlock) error {
+		latestSignals.Set(seat, signaledBlock)
+
+		return nil
+	}); err != nil {
+		return ierrors.Wrap(err, "failed to restore upgrade signals from disk")
+	}
+
+	return nil
+}
