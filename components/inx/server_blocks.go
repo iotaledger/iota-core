5c2cf6822701c251fd03677b4dffa063282496fa
Alexander Sporn <github@alexsporn.de>
2023-07-25 12:53:53 +0200

    Added SubmitPayload to inx

--- components/inx/server_blocks.go
+++ components/inx/server_blocks.go
@@ -14,6 +14,7 @@ import (
 	inx "github.com/iotaledger/inx/go"
 	"github.com/iotaledger/iota-core/pkg/blockfactory"
 	"github.com/iotaledger/iota-core/pkg/protocol/engine/blocks"
+	iotago "github.com/iotaledger/iota.go/v4"
 )
 
 func (s *Server) ReadBlock(_ context.Context, blockID *inx.BlockId) (*inx.RawBlock, error) {
@@ -132,11 +133,42 @@ func (s *Server) ListenToConfirmedBlocks(_ *inx.NoParams, srv inx.INX_ListenToCo
 }
 
 func (s *Server) SubmitBlock(ctx context.Context, rawBlock *inx.RawBlock) (*inx.BlockId, error) {
-	block, err := rawBlock.UnwrapBlock(deps.Protocol.LatestAPI(), serix.WithValidation())
+	version, _, err := iotago.VersionFromBytes(rawBlock.GetData())
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "failed to parse block version: %s", err.Error())
+	}
+
+	apiForVersion, err := deps.Protocol.APIForVersion(version)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid block version: %s", err.Error())
+	}
+
+	block, err := rawBlock.UnwrapBlock(apiForVersion, serix.WithValidation())
 	if err != nil {
 		return nil, err
 	}
 
+	return s.attachBlock(ctx, block)
+}
+
+func (s *Server) SubmitPayload(ctx context.Context, rawPayload *inx.RawPayload) (*inx.BlockId, error) {
+	payload, err := rawPayload.Unwrap(deps.Protocol.CurrentAPI(), serix.WithValidation())
+	if err != nil {
+		return nil, err
+	}
+
+	mergedCtx, mergedCtxCancel := contextutils.MergeContexts(ctx, Component.Daemon().ContextStopped())
+	defer mergedCtxCancel()
+
+	block, err := deps.BlockIssuer.CreateBlock(mergedCtx, blockfactory.WithPayload(payload))
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "failed to create block: %s", err.Error())
+	}
+
+	return s.attachBlock(ctx, block.ProtocolBlock())
+}
+
+func (s *Server) attachBlock(ctx context.Context, block *iotago.ProtocolBlock) (*inx.BlockId, error) {
 	mergedCtx, mergedCtxCancel := contextutils.MergeContexts(ctx, Component.Daemon().ContextStopped())
 	defer mergedCtxCancel()
 
