03bf0331be07fdb856dc9449405f352ab28098bf
Andrea V <1577639+karimodm@users.noreply.github.com>
2023-07-26 15:02:55 +0200

    Online Committee Seats

--- components/metrics/metrics_accounts.go
+++ components/metrics/metrics_accounts.go
@@ -1,8 +1,6 @@
 package metrics
 
 import (
-	"fmt"
-
 	"github.com/iotaledger/hive.go/runtime/event"
 	"github.com/iotaledger/iota-core/components/metrics/collector"
 	"github.com/iotaledger/iota-core/pkg/protocol/engine/blocks"
@@ -11,23 +9,30 @@ import (
 const (
 	accountNamespace = "account"
 
-	credits = "credits"
+	credits     = "credits"
+	activeSeats = "active_seats"
 )
 
 var AccountMetrics = collector.NewCollection(accountNamespace,
 	collector.WithMetric(collector.NewMetric(credits,
 		collector.WithType(collector.GaugeVec),
-		collector.WithHelp("Time since transaction issuance to the conflict acceptance"),
+		collector.WithHelp("Credits per Account."),
 		collector.WithLabels("account"),
 		collector.WithLabelValuesCollection(),
 		collector.WithInitFunc(func() {
 			deps.Protocol.Events.Engine.BlockGadget.BlockAccepted.Hook(func(block *blocks.Block) {
 				accountData, exists, _ := deps.Protocol.MainEngineInstance().Ledger.Account(block.ProtocolBlock().IssuerID, deps.Protocol.SyncManager.LatestCommittedSlot())
 				if exists {
-					fmt.Println(">>", accountData.ID.String(), accountData.Credits.Value)
 					deps.Collector.Update(accountNamespace, credits, collector.MultiLabels(accountData.ID.String()), float64(accountData.Credits.Value))
 				}
 			}, event.WithWorkerPool(Component.WorkerPool))
+		}),
+	)),
+	collector.WithMetric(collector.NewMetric(activeSeats,
+		collector.WithType(collector.Gauge),
+		collector.WithHelp("Seats seen as active by the node."),
+		collector.WithCollectFunc(func() map[string]float64 {
+				return collector.SingleValue(deps.Protocol.MainEngineInstance().SybilProtection.SeatManager().OnlineCommittee().Size())
 		}),
 	)),
 )
