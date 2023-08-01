2b55a714dfe7a945d54a22f7ba47e6fe6cfbdd89
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-08-01 13:25:59 +0800

    Remove future blocks from account filter

--- pkg/protocol/engine/commitmentfilter/accountsfilter/commitmentfilter.go
+++ pkg/protocol/engine/commitmentfilter/accountsfilter/commitmentfilter.go
@@ -1,14 +1,10 @@
 package accountsfilter
 
 import (
-	"fmt"
-	"sync"
-
 	"github.com/iotaledger/hive.go/core/memstorage"
 	hiveEd25519 "github.com/iotaledger/hive.go/crypto/ed25519"
 	"github.com/iotaledger/hive.go/ds"
 	"github.com/iotaledger/hive.go/ierrors"
-	"github.com/iotaledger/hive.go/lo"
 	"github.com/iotaledger/hive.go/runtime/module"
 	"github.com/iotaledger/hive.go/runtime/options"
 	"github.com/iotaledger/inx-app/pkg/api"
@@ -16,7 +12,6 @@ import (
 	"github.com/iotaledger/iota-core/pkg/protocol/engine"
 	"github.com/iotaledger/iota-core/pkg/protocol/engine/accounts"
 	"github.com/iotaledger/iota-core/pkg/protocol/engine/commitmentfilter"
-	"github.com/iotaledger/iota-core/pkg/protocol/engine/notarization"
 	iotago "github.com/iotaledger/iota.go/v4"
 )
 
@@ -39,10 +34,6 @@ type CommitmentFilter struct {
 
 	accountRetrieveFunc func(accountID iotago.AccountID, targetIndex iotago.SlotIndex) (*accounts.AccountData, bool, error)
 
-	futureBlocksMutex sync.RWMutex
-
-	nextIndexToPromote iotago.SlotIndex
-
 	module.Module
 }
 
@@ -54,10 +45,6 @@ func NewProvider(opts ...options.Option[CommitmentFilter]) module.Provider[*engi
 			c.commitmentFunc = e.Storage.Commitments().Load
 
 			c.accountRetrieveFunc = e.Ledger.Account
-
-			e.Events.Notarization.SlotCommitted.Hook(func(details *notarization.SlotCommittedDetails) {
-				c.PromoteFutureBlocksUntil(details.Commitment.Index())
-			})
 
 			e.Events.Filter.BlockPreAllowed.Hook(func(block *model.Block) {
 				c.ProcessPreFilteredBlock(block)
@@ -81,51 +68,7 @@ func New(apiProvider api.Provider, opts ...options.Option[CommitmentFilter]) *Co
 	)
 }
 
-func (c *CommitmentFilter) PromoteFutureBlocksUntil(index iotago.SlotIndex) {
-	c.futureBlocksMutex.Lock()
-	defer c.futureBlocksMutex.Unlock()
-
-	for i := c.nextIndexToPromote; i <= index; i++ {
-		cm, err := c.commitmentFunc(i)
-		if err != nil {
-			panic(fmt.Sprintf("failed to load commitment for index %d: %s", i, err))
-		}
-		if storage := c.futureBlocks.Get(i, false); storage != nil {
-			if futureBlocks, exists := storage.Get(cm.ID()); exists {
-				_ = futureBlocks.ForEach(func(futureBlock *model.Block) (err error) {
-					c.ProcessPreFilteredBlock(futureBlock)
-					return nil
-				})
-			}
-		}
-		c.futureBlocks.Evict(i)
-	}
-
-	c.nextIndexToPromote = index + 1
-}
-
-func (c *CommitmentFilter) isFutureBlock(block *model.Block) (isFutureBlock bool) {
-	c.futureBlocksMutex.RLock()
-	defer c.futureBlocksMutex.RUnlock()
-
-	// If we are not able to load the commitment for the block, it means we haven't committed this slot yet.
-	if _, err := c.commitmentFunc(block.ProtocolBlock().SlotCommitmentID.Index()); err != nil {
-		lo.Return1(c.futureBlocks.Get(block.ProtocolBlock().SlotCommitmentID.Index(), true).GetOrCreate(block.ProtocolBlock().SlotCommitmentID, func() ds.Set[*model.Block] {
-			return ds.NewSet[*model.Block]()
-		})).Add(block)
-
-		return true
-	}
-
-	return false
-}
-
 func (c *CommitmentFilter) ProcessPreFilteredBlock(block *model.Block) {
-	// TODO: redo optimisations to mark future block rather than checking each time.
-	if c.isFutureBlock(block) {
-		return
-	}
-
 	// check if the account exists in the specified slot.
 	accountData, exists, err := c.accountRetrieveFunc(block.ProtocolBlock().IssuerID, block.ProtocolBlock().SlotCommitmentID.Index())
 	if err != nil {
