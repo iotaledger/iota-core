bba7ccd95691a947d9b61d6a3dfa959e3162a55f
jkrvivian <jkrvivian@gmail.com>
2023-08-02 20:18:58 +0800

    Include validator pool details in debug api

--- components/debugapi/node.go
+++ components/debugapi/node.go
@@ -11,18 +11,23 @@ func validatorsSummary() (*ValidatorsSummaryResponse, error) {
 	seatManager := deps.Protocol.MainEngineInstance().SybilProtection.SeatManager()
 	latestSlotIndex := deps.Protocol.MainEngineInstance().Storage.Settings().LatestCommitment().Index()
 	latestCommittee := seatManager.Committee(latestSlotIndex)
-	validatorSeats := make(map[int]string)
+	validatorSeats := []*Validator{}
 	latestCommittee.Accounts().ForEach(func(id iotago.AccountID, pool *account.Pool) bool {
-		// TODO: check if seat exists, and include Pool info in the response
-		validatorSeats[int(lo.Return1(latestCommittee.GetSeat(id)))] = id.String()
+		validatorSeats = append(validatorSeats, &Validator{
+			AccountID:      id,
+			SeatIndex:      uint8(lo.Return1(latestCommittee.GetSeat(id))),
+			PoolStake:      pool.PoolStake,
+			ValidatorStake: pool.ValidatorStake,
+			FixedCost:      pool.FixedCost,
+		})
 
 		return true
 	})
 
 	return &ValidatorsSummaryResponse{
 		ValidatorSeats: validatorSeats,
-		ActiveSeats: lo.Map(seatManager.OnlineCommittee().ToSlice(), func(seatIndex account.SeatIndex) int {
-			return int(seatIndex)
+		ActiveSeats: lo.Map(seatManager.OnlineCommittee().ToSlice(), func(seatIndex account.SeatIndex) uint32 {
+			return uint32(seatIndex)
 		}),
 	}, nil
 }
