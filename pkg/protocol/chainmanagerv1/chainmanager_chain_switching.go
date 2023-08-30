fea8d38439845fc219c45c869be1563523e4f0e7
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-08-30 15:59:05 +0200

    Feat: started working on chain switching stub

--- pkg/protocol/chainmanagerv1/chainmanager_chain_switching.go
+++ pkg/protocol/chainmanagerv1/chainmanager_chain_switching.go
@@ -12,25 +12,40 @@ func newChainManagerChainSwitching(chainManager *ChainManager) *chainManagerChai
 	}
 
 	heaviestClaimedCandidate := reactive.NewVariable[*Chain]()
+	heaviestClaimedCandidate.OnUpdate(func(prevCandidate, newCandidate *Chain) {
+		if prevCandidate != nil {
+			// TODO: STOP REQUESTING ATTESTATIONS FOR OLD CANDIDATE
+		}
+
+		// TODO: REQUEST ATTESTATIONS FOR NEW CANDIDATE
+	})
+
 	heaviestAttestedCandidate := reactive.NewVariable[*Chain]()
+	heaviestAttestedCandidate.OnUpdate(func(prevCandidate, newCandidate *Chain) {
+		if prevCandidate != nil {
+			// TODO: STOP SYNCING OLD CANDIDATE
+		}
 
-	selectHeaviestCandidate := func(variable reactive.Variable[*Chain], newCandidate *Chain, chainWeight func(*Chain) reactive.Variable[uint64]) {
-		chainWeight(newCandidate).OnUpdate(func(_, newChainWeight uint64) {
-			if newChainWeight <= chainManager.mainChain.Get().verifiedWeight.Get() {
-				return
-			}
-
-			variable.Compute(func(currentCandidate *Chain) *Chain {
-				if currentCandidate == nil || currentCandidate.evicted.WasTriggered() || newChainWeight > chainWeight(currentCandidate).Get() {
-					return newCandidate
-				}
-
-				return currentCandidate
-			})
-		})
-	}
+		// TODO: START SYNCING NEW CANDIDATE
+	})
 
 	chainManager.OnChainCreated(func(chain *Chain) {
+		selectHeaviestCandidate := func(candidate reactive.Variable[*Chain], newCandidate *Chain, chainWeight func(*Chain) reactive.Variable[uint64]) {
+			chainWeight(newCandidate).OnUpdate(func(_, newChainWeight uint64) {
+				if newChainWeight <= chainManager.mainChain.Get().verifiedWeight.Get() {
+					return
+				}
+
+				candidate.Compute(func(currentCandidate *Chain) *Chain {
+					if currentCandidate == nil || currentCandidate.evicted.WasTriggered() || newChainWeight > chainWeight(currentCandidate).Get() {
+						return newCandidate
+					}
+
+					return currentCandidate
+				})
+			})
+		}
+
 		selectHeaviestCandidate(heaviestClaimedCandidate, chain, (*Chain).ClaimedWeight)
 		selectHeaviestCandidate(heaviestAttestedCandidate, chain, (*Chain).AttestedWeight)
 		selectHeaviestCandidate(c.candidateChain, chain, (*Chain).VerifiedWeight)
