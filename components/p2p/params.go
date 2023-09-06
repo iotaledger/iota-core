9f668629e5be2a4badd0cac806dfbef798e67947
Andrea V <1577639+karimodm@users.noreply.github.com>
2023-09-06 12:12:47 +0300

    ip6 bind address addressed in other PR

--- components/p2p/params.go
+++ components/p2p/params.go
@@ -7,7 +7,7 @@ import (
 // ParametersP2P contains the definition of configuration parameters used by the p2p plugin.
 type ParametersP2P struct {
 	// BindAddress defines on which multi address the p2p service should listen on.
-	BindAddress string `default:"/ip4/0.0.0.0/tcp/14666,/ip6/::/tcp/14666" usage:"the multi address for p2p connections"`
+	BindAddress string `default:"/ip4/0.0.0.0/tcp/14666" usage:"the multi address for p2p connections"`
 
 	ConnectionManager struct {
 		// Defines the high watermark to use within the connection manager.
