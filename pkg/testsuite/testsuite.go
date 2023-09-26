a05c9d79ede4186f2fa7ec3a6b06c55ee1e5ddcf
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2023-09-26 18:30:33 +0200

    Fix linter

--- pkg/testsuite/testsuite.go
+++ pkg/testsuite/testsuite.go
@@ -354,7 +354,7 @@ func (t *TestSuite) addNodeToPartition(name string, partition string, validator 
 			Mana:                 iotago.Mana(amount),
 			IssuerKey:            iotago.Ed25519PublicKeyBlockIssuerKeyFromPublicKey(ed25519.PublicKey(node.PubKey)),
 			ExpirySlot:           iotago.MaxSlotIndex,
-			BlockIssuanceCredits: iotago.BlockIssuanceCredits(iotago.MaxBlockIssuanceCredits),
+			BlockIssuanceCredits: iotago.MaxBlockIssuanceCredits,
 		}
 		if validator {
 			accountDetails.StakedAmount = accountDetails.Amount
