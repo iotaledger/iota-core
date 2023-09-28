44742d14de2518273ace2c618006a5abebfef0e5
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-09-28 11:54:17 +0200

    Update to latest iota.go

--- tools/genesis-snapshot/presets/presets.go
+++ tools/genesis-snapshot/presets/presets.go
@@ -23,7 +23,7 @@ var Base = []options.Option[snapshotcreator.Options]{
 			iotago.WithNetworkOptions("default", "rms"),
 			iotago.WithSupplyOptions(10_000_000_000, 100, 1, 10, 100, 100, 100),
 			iotago.WithTimeProviderOptions(time.Now().Unix(), 10, 13),
-			iotago.WithLivenessOptions(50*time.Second, 50*time.Second, 7, 14, 30),
+			iotago.WithLivenessOptions(30, 30, 7, 14, 30),
 			// increase/decrease threshold = fraction * slotDurationInSeconds * schedulerRate
 			iotago.WithCongestionControlOptions(500, 500, 500, 800000, 500000, 100000, 1, 1000, 100),
 			iotago.WithWorkScoreOptions(25, 1, 10, 100, 50, 10, 10, 50, 1, 10, 250, 2),
@@ -97,7 +97,7 @@ var Docker = []options.Option[snapshotcreator.Options]{
 			iotago.WithNetworkOptions("docker", "rms"),
 			iotago.WithSupplyOptions(10_000_000_000, 1, 1, 10, 100, 100, 100),
 			iotago.WithTimeProviderOptions(time.Now().Unix(), 10, 13),
-			iotago.WithLivenessOptions(50*time.Second, 50*time.Second, 7, 14, 30),
+			iotago.WithLivenessOptions(30, 30, 7, 14, 30),
 			// increase/decrease threshold = fraction * slotDurationInSeconds * schedulerRate
 			iotago.WithCongestionControlOptions(500, 500, 500, 800000, 500000, 100000, 1, 1000, 100),
 			iotago.WithWorkScoreOptions(25, 1, 10, 100, 50, 10, 10, 50, 1, 10, 250, 2),
@@ -151,7 +151,7 @@ var Feature = []options.Option[snapshotcreator.Options]{
 			iotago.WithNetworkOptions("feature", "rms"),
 			iotago.WithSupplyOptions(10_000_000_000, 100, 1, 10, 100, 100, 100),
 			iotago.WithTimeProviderOptions(1689848996, 10, 13),
-			iotago.WithLivenessOptions(50*time.Second, 50*time.Second, 10, 20, 30),
+			iotago.WithLivenessOptions(30, 30, 10, 20, 30),
 			// increase/decrease threshold = fraction * slotDurationInSeconds * schedulerRate
 			iotago.WithCongestionControlOptions(500, 500, 500, 800000, 500000, 100000, 1, 1000, 100),
 			iotago.WithWorkScoreOptions(25, 1, 10, 100, 50, 10, 10, 50, 1, 10, 250, 2),
