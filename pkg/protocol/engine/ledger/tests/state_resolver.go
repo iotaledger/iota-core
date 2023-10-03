3696eec95bf0843d29c7a104ca3ccff34a3efda5
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-10-03 18:56:26 +0200

    Refactor: reverted changes

--- pkg/protocol/engine/ledger/tests/state_resolver.go
+++ pkg/protocol/engine/ledger/tests/state_resolver.go
@@ -5,7 +5,6 @@ import (
 	"github.com/iotaledger/hive.go/ierrors"
 	"github.com/iotaledger/iota-core/pkg/core/promise"
 	"github.com/iotaledger/iota-core/pkg/protocol/engine/mempool"
-	iotago "github.com/iotaledger/iota.go/v4"
 )
 
 type MockStateResolver struct {
@@ -31,29 +30,13 @@ func (s *MockStateResolver) DestroyOutputState(stateID mempool.StateID) {
 	s.statesByID.Delete(stateID)
 }
 
-func (s *MockStateResolver) ResolveOutputState(reference iotago.Input) *promise.Promise[mempool.State] {
-	if reference.Type() == iotago.InputUTXO {
-		output, exists := s.statesByID.Get(reference.ReferencedStateID())
-		if !exists {
-			return promise.New[mempool.State]().Reject(ierrors.Errorf("output %s not found: %w", reference.ReferencedStateID().ToHex(), mempool.ErrStateNotFound))
-		}
-
-		return promise.New[mempool.State]().Resolve(output)
-	} else if reference.Type() == iotago.InputCommitment {
-		//nolint:forcetypeassert
-		output := &iotago.Commitment{
-			ProtocolVersion:      0,
-			Slot:                 0,
-			PreviousCommitmentID: reference.(*iotago.CommitmentInput).CommitmentID,
-			RootsID:              iotago.Identifier{},
-			CumulativeWeight:     0,
-			ReferenceManaCost:    0,
-		}
-
-		return promise.New[mempool.State]().Resolve(output)
+func (s *MockStateResolver) ResolveOutputState(outputID mempool.StateID) *promise.Promise[mempool.State] {
+	output, exists := s.statesByID.Get(outputID)
+	if !exists {
+		return promise.New[mempool.State]().Reject(ierrors.Errorf("output %s not found: %w", outputID.ToHex(), mempool.ErrStateNotFound))
 	}
 
-	return promise.New[mempool.State]().Reject(ierrors.Errorf("state not found"))
+	return promise.New[mempool.State]().Resolve(output)
 }
 
 func (s *MockStateResolver) Cleanup() {
