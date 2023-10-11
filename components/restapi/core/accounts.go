8e7b032d5d8754225d4b0a3cd1263d92f2ae999c
Daria Dziubałtowska <daria305@gmail.com>
2023-10-11 13:47:51 +0200

    Fix RMC API nil pointer

--- components/restapi/core/accounts.go
+++ components/restapi/core/accounts.go
@@ -36,7 +36,7 @@ func congestionForAccountID(c echo.Context) (*apimodels.CongestionResponse, erro
 	if err != nil {
 		rmcSlot = 0
 	}
-	rmc, err := deps.Protocol.CandidateEngineInstance().Ledger.RMCManager().RMC(rmcSlot)
+	rmc, err := deps.Protocol.MainEngineInstance().Ledger.RMCManager().RMC(rmcSlot)
 	if err != nil {
 		return nil, ierrors.Wrapf(err, "failed to get RMC for slot: %d", rmcSlot)
 	}
