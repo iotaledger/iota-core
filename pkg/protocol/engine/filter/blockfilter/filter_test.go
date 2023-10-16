41fed0f8cfe7aba2e868e2ca6354ce0880e09c52
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-10-16 11:36:33 +0800

    Reintroduce filter for block version so that only blocks with the correct/matching version for an epoch are seen as valid. This is important as otherwise it is possible to issue blocks with a higher version before the version is activated or continue issuing blocks with the old version.

--- pkg/protocol/engine/filter/blockfilter/filter_test.go
+++ pkg/protocol/engine/filter/blockfilter/filter_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/iotaledger/hive.go/ds"
 	"github.com/iotaledger/hive.go/ierrors"
 	"github.com/iotaledger/hive.go/runtime/options"
 	"github.com/iotaledger/hive.go/serializer/v2/serix"
@@ -79,6 +80,22 @@ func (t *TestFramework) IssueValidationBlockAtTime(alias string, issuingTime tim
 	return t.processBlock(alias, block)
 }
 
+func (t *TestFramework) IssueBlockAtSlotWithVersion(alias string, slot iotago.SlotIndex, version iotago.Version, apiProvider iotago.APIProvider) error {
+	apiForVersion, err := apiProvider.APIForVersion(version)
+	require.NoError(t.Test, err)
+
+	block, err := builder.NewBasicBlockBuilder(apiForVersion).
+		ProtocolVersion(version).
+		StrongParents(iotago.BlockIDs{iotago.BlockID{}}).
+		StrongParents(iotago.BlockIDs{tpkg.RandBlockID()}).
+		IssuingTime(apiForVersion.TimeProvider().SlotStartTime(slot)).
+		SlotCommitmentID(iotago.NewCommitment(apiForVersion.Version(), slot-apiForVersion.ProtocolParameters().MinCommittableAge(), iotago.CommitmentID{}, iotago.Identifier{}, 0, 0).MustID()).
+		Build()
+	require.NoError(t.Test, err)
+
+	return t.processBlock(alias, block)
+}
+
 func mockedCommitteeFunc(validatorAccountID iotago.AccountID) func(iotago.SlotIndex) *account.SeatedAccounts {
 	mockedAccounts := account.NewAccounts()
 	mockedAccounts.Set(validatorAccountID, new(account.Pool))
@@ -113,6 +130,72 @@ func TestFilter_WithMaxAllowedWallClockDrift(t *testing.T) {
 	require.NoError(t, tf.IssueUnsignedBlockAtTime("present", time.Now()))
 	require.NoError(t, tf.IssueUnsignedBlockAtTime("acceptedFuture", time.Now().Add(allowedDrift)))
 	require.NoError(t, tf.IssueUnsignedBlockAtTime("tooFarAheadFuture", time.Now().Add(allowedDrift).Add(1*time.Second)))
+}
+
+func TestFilter_ProtocolVersion(t *testing.T) {
+	apiProvider := api.NewEpochBasedProvider(
+		api.WithAPIForMissingVersionCallback(
+			func(params iotago.ProtocolParameters) (iotago.API, error) {
+				return iotago.V3API(iotago.NewV3ProtocolParameters(iotago.WithVersion(params.Version()))), nil
+			},
+		),
+	)
+	apiProvider.AddProtocolParametersAtEpoch(iotago.NewV3ProtocolParameters(iotago.WithVersion(3)), 0)
+	apiProvider.AddProtocolParametersAtEpoch(iotago.NewV3ProtocolParameters(iotago.WithVersion(4)), 3)
+
+	timeProvider := apiProvider.CurrentAPI().TimeProvider()
+
+	tf := NewTestFramework(t,
+		apiProvider,
+		// Set this to some value far in the future so that we can arbitrarily manipulate block times.
+		WithMaxAllowedWallClockDrift(time.Duration(uint64(timeProvider.EpochEnd(50))*uint64(timeProvider.SlotDurationSeconds()))*time.Second),
+	)
+
+	valid := ds.NewSet[string]()
+	invalid := ds.NewSet[string]()
+
+	tf.Filter.events.BlockPreAllowed.Hook(func(block *model.Block) {
+		require.True(t, valid.Has(block.ID().Alias()))
+		require.False(t, invalid.Has(block.ID().Alias()))
+	})
+
+	tf.Filter.events.BlockPreFiltered.Hook(func(event *filter.BlockPreFilteredEvent) {
+		block := event.Block
+		require.False(t, valid.Has(block.ID().Alias()))
+		require.True(t, invalid.Has(block.ID().Alias()))
+		require.True(t, ierrors.Is(event.Reason, ErrInvalidBlockVersion))
+	})
+
+	invalid.Add("A")
+	require.NoError(t, tf.IssueBlockAtSlotWithVersion("A", timeProvider.EpochStart(1), 4, apiProvider))
+
+	valid.Add("B")
+	require.NoError(t, tf.IssueBlockAtSlotWithVersion("B", timeProvider.EpochEnd(1), 3, apiProvider))
+
+	valid.Add("C")
+	require.NoError(t, tf.IssueBlockAtSlotWithVersion("C", timeProvider.EpochEnd(2), 3, apiProvider))
+
+	invalid.Add("D")
+	require.NoError(t, tf.IssueBlockAtSlotWithVersion("D", timeProvider.EpochStart(3), 3, apiProvider))
+
+	valid.Add("E")
+	require.NoError(t, tf.IssueBlockAtSlotWithVersion("E", timeProvider.EpochStart(3), 4, apiProvider))
+	valid.Add("F")
+	require.NoError(t, tf.IssueBlockAtSlotWithVersion("F", timeProvider.EpochEnd(3), 4, apiProvider))
+
+	valid.Add("G")
+	require.NoError(t, tf.IssueBlockAtSlotWithVersion("G", timeProvider.EpochStart(5)+5, 4, apiProvider))
+
+	apiProvider.AddProtocolParametersAtEpoch(iotago.NewV3ProtocolParameters(iotago.WithVersion(5)), 10)
+
+	valid.Add("H")
+	require.NoError(t, tf.IssueBlockAtSlotWithVersion("H", timeProvider.EpochEnd(9), 4, apiProvider))
+
+	invalid.Add("I")
+	require.NoError(t, tf.IssueBlockAtSlotWithVersion("I", timeProvider.EpochStart(10), 4, apiProvider))
+
+	valid.Add("J")
+	require.NoError(t, tf.IssueBlockAtSlotWithVersion("J", timeProvider.EpochStart(10), 5, apiProvider))
 }
 
 func TestFilter_ValidationBlocks(t *testing.T) {
