3126a4133b90efcbc6c06caab12a7cacfd1fee38
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-11-07 21:22:05 +0100

    Feat: cleaned up code

--- pkg/protocol/constants.go
+++ pkg/protocol/constants.go
@@ -3,11 +3,4 @@ package protocol
 const (
 	// DatabaseVersion defines the current version of the database.
 	DatabaseVersion byte = 1
-
-	// SyncWindow defines the maximum amount of slots that a node requests on top of its latest verified commitment.
-	SyncWindow = 1
-
-	// WarpSyncOffset defines how many slots a commitment needs to be behind the latest commitment to be requested by
-	// the warp sync process.
-	WarpSyncOffset = 1
 )
