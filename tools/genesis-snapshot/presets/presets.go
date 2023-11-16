441589e80113999f409007dcec87b3ae1d1da529
Andrea V <1577639+karimodm@users.noreply.github.com>
2023-11-16 10:09:45 +0100

    Feature snapshot genesis time at 10_000 slots in the past

--- tools/genesis-snapshot/presets/presets.go
+++ tools/genesis-snapshot/presets/presets.go
@@ -242,7 +242,7 @@ var Feature = []options.Option[snapshotcreator.Options]{
 		iotago.NewV3ProtocolParameters(
 			iotago.WithNetworkOptions("feature", "rms"),
 			iotago.WithSupplyOptions(4_600_000_000_000_000, 100, 1, 10, 100, 100, 100),
-			iotago.WithTimeProviderOptions(666666, time.Now().Unix(), 10, 13),
+			iotago.WithTimeProviderOptions(666666, time.Now().Unix()-100_000, 10, 13), // Let's fix genesis at 10_000 slots back.
 			iotago.WithLivenessOptions(30, 30, 10, 20, 30),
 			// increase/decrease threshold = fraction * slotDurationInSeconds * schedulerRate
 			iotago.WithCongestionControlOptions(500, 500, 500, 800000, 500000, 100000, 1000, 100),
