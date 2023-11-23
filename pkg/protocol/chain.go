69761534fb109cdd8d3243aee82a3f1388eb7e6b
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-11-23 04:00:04 +0100

    Refactor: removed unused code

--- pkg/protocol/chain.go
+++ pkg/protocol/chain.go
@@ -204,10 +204,6 @@ func (c *Chain) initLogging(chains *Chains) (self *Chain) {
 func (c *Chain) initBehavior(chains *Chains) (self *Chain) {
 	teardownBehavior := lo.Batch(
 		c.Parent.WithNonEmptyValue(func(parent *Chain) (teardown func()) {
-			if parent == c {
-				c.LogError("WHAT IS GOING ON?!?")
-			}
-
 			parent.Children.Add(c)
 
 			return func() {
@@ -249,7 +245,7 @@ func (c *Chain) initBehavior(chains *Chains) (self *Chain) {
 			}
 
 			return forkingPoint.Parent.WithValue(func(parentCommitment *Commitment) (teardown func()) {
-				if parentCommitment == nil || parentCommitment.IsRoot.Get() {
+				if parentCommitment == nil {
 					c.Parent.Set(nil)
 
 					return func() {}
