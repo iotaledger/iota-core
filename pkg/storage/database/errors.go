a86b55eecacb073d31aa015c30fe94d1d0cd2868
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2023-12-04 16:01:24 +0100

    KVStore returns error when trying to access a shutdown DBInstance

--- pkg/storage/database/errors.go
+++ pkg/storage/database/errors.go
@@ -3,7 +3,9 @@ package database
 import "github.com/iotaledger/hive.go/ierrors"
 
 var (
-	ErrEpochPruned     = ierrors.New("epoch pruned")
-	ErrNoPruningNeeded = ierrors.New("no pruning needed")
-	ErrDatabaseFull    = ierrors.New("database full")
+	ErrEpochPruned       = ierrors.New("epoch pruned")
+	ErrNoPruningNeeded   = ierrors.New("no pruning needed")
+	ErrDatabaseFull      = ierrors.New("database full")
+	ErrDatabaseShutdown  = ierrors.New("cannot open DBInstance that is shutdown")
+	ErrDatabaseNotClosed = ierrors.New("cannot open DBInstance that is not closed")
 )
