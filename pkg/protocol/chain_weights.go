6c7537094a49dcd9e1f3da1d8d42825d9c7fc2d2
Hans Moog <3293976+hmoog@users.noreply.github.com>
2024-01-15 01:36:54 +0100

    Feat: refactored code

--- pkg/protocol/chain_weights.go
+++ pkg/protocol/chain_weights.go
@@ -3,56 +3,116 @@ package protocol
 import (
 	"github.com/iotaledger/hive.go/ds/reactive"
 	"github.com/iotaledger/hive.go/ds/shrinkingmap"
+	"github.com/iotaledger/hive.go/lo"
 	iotago "github.com/iotaledger/iota.go/v4"
 )
 
-type ChainWeights struct {
-	WeightReference reactive.Variable[iotago.SlotIndex]
+type HeaviestCandidates struct {
+	ClaimedWeight *HeaviestCandidate
 
-	Claimed *shrinkingmap.ShrinkingMap[iotago.SlotIndex, reactive.SortedSet[*Commitment]]
+	AttestedWeight *HeaviestCandidate
 
-	Attested *shrinkingmap.ShrinkingMap[iotago.SlotIndex, reactive.SortedSet[*Commitment]]
+	VerifiedWeight *HeaviestCandidate
 
-	Verified *shrinkingmap.ShrinkingMap[iotago.SlotIndex, reactive.SortedSet[*Commitment]]
+	measuredSlot reactive.Variable[iotago.SlotIndex]
 
 	reactive.EvictionState[iotago.SlotIndex]
 }
 
-func NewChainWeights() *ChainWeights {
-	c := &ChainWeights{
-		WeightReference: reactive.NewVariable[iotago.SlotIndex](increasing[iotago.SlotIndex]),
-		Claimed:         shrinkingmap.New[iotago.SlotIndex, reactive.SortedSet[*Commitment]](),
-		Attested:        shrinkingmap.New[iotago.SlotIndex, reactive.SortedSet[*Commitment]](),
-		Verified:        shrinkingmap.New[iotago.SlotIndex, reactive.SortedSet[*Commitment]](),
+func NewHeaviestCandidates() *HeaviestCandidates {
+	c := &HeaviestCandidates{
+		measuredSlot:   reactive.NewVariable[iotago.SlotIndex](increasing[iotago.SlotIndex]),
+		ClaimedWeight:  newHeaviestCandidate((*Commitment).weightAddr),
+		AttestedWeight: newHeaviestCandidate((*Commitment).weightAddr),
+		VerifiedWeight: newHeaviestCandidate((*Commitment).weightAddr),
 	}
+
+	c.measuredSlot.WithNonEmptyValue(c.updateCandidates)
 
 	return c
 }
 
-func (c *ChainWeights) Track(chain *Chain) (teardown func()) {
+func (c *HeaviestCandidates) Track(chain *Chain) (teardown func()) {
 	return chain.LatestCommitment.OnUpdate(func(_ *Commitment, latestCommitment *Commitment) {
-		if targetSlot := latestCommitment.ID().Index(); !c.EvictionEvent(targetSlot).WasTriggered() {
-			c.weightedCommitments(c.Claimed, targetSlot).Add(latestCommitment)
-			c.weightedCommitments(c.Attested, targetSlot).Add(latestCommitment)
-			c.weightedCommitments(c.Verified, targetSlot).Add(latestCommitment)
+		targetSlot := latestCommitment.ID().Index()
 
-			c.WeightReference.Compute(func(currentValue iotago.SlotIndex) iotago.SlotIndex {
-				return max(currentValue, targetSlot-1)
-			})
+		if evictionEvent := c.EvictionEvent(targetSlot); !evictionEvent.WasTriggered() {
+			c.ClaimedWeight.registerCommitment(targetSlot, latestCommitment, evictionEvent)
+			c.AttestedWeight.registerCommitment(targetSlot, latestCommitment, evictionEvent)
+			c.VerifiedWeight.registerCommitment(targetSlot, latestCommitment, evictionEvent)
+
+			c.measuredSlot.Set(targetSlot - measurementOffset)
 		}
 	})
 }
 
-func (c *ChainWeights) weightedCommitments(targetMap *shrinkingmap.ShrinkingMap[iotago.SlotIndex, reactive.SortedSet[*Commitment]], targetSlot iotago.SlotIndex) reactive.SortedSet[*Commitment] {
-	weightedCommitments, slotCreated := targetMap.GetOrCreate(targetSlot, func() reactive.SortedSet[*Commitment] {
-		return reactive.NewSortedSet((*Commitment).weightAddr)
-	})
+func (c *HeaviestCandidates) updateCandidates(slot iotago.SlotIndex) (teardown func()) {
+	return lo.Batch(
+		c.ClaimedWeight.update(slot),
+		c.AttestedWeight.update(slot),
+		c.VerifiedWeight.update(slot),
+	)
+}
 
+type HeaviestCandidate struct {
+	reactive.Variable[*Chain]
+
+	weightedCommitmentsBySlot *shrinkingmap.ShrinkingMap[iotago.SlotIndex, reactive.SortedSet[*Commitment]]
+
+	sortedSetFactory func() reactive.SortedSet[*Commitment]
+}
+
+func newHeaviestCandidate(weightVariable func(element *Commitment) reactive.Variable[uint64]) *HeaviestCandidate {
+	return &HeaviestCandidate{
+		Variable:                  reactive.NewVariable[*Chain](),
+		weightedCommitmentsBySlot: shrinkingmap.New[iotago.SlotIndex, reactive.SortedSet[*Commitment]](),
+		sortedSetFactory: func() reactive.SortedSet[*Commitment] {
+			return reactive.NewSortedSet(weightVariable)
+		},
+	}
+}
+
+func (c *HeaviestCandidate) update(slot iotago.SlotIndex) func() {
+	return lo.Return1(c.weightedCommitmentsBySlot.Get(slot)).HeaviestElement().WithNonEmptyValue(func(heaviestCommitment *Commitment) (teardown func()) {
+		heaviestChain := heaviestCommitment.Chain.Get()
+		if true /* heaviest chain is not equal main chain */ {
+			slotsWithHeaviestChain := reactive.NewCounter[*Commitment](func(commitment *Commitment) bool {
+				return commitment.Chain.Get() == heaviestChain
+			})
+
+			unsubscribeFromCounter := slotsWithHeaviestChain.OnUpdate(func(_ int, slotsWithHeaviestChain int) {
+				if iotago.SlotIndex(slotsWithHeaviestChain) >= chainSwitchingThreshold-1 {
+					c.Set(heaviestChain)
+				} else {
+					c.Set(nil)
+				}
+			})
+
+			teardownFunctions := []func(){unsubscribeFromCounter}
+			for i := iotago.SlotIndex(1); i < chainSwitchingThreshold; i++ {
+				if commitments, commitmentsExist := c.weightedCommitmentsBySlot.Get(slot - i); commitmentsExist {
+					teardownFunctions = append(teardownFunctions, slotsWithHeaviestChain.Monitor(commitments.HeaviestElement()))
+				}
+			}
+
+			teardown = lo.Batch(teardownFunctions...)
+		}
+
+		return teardown
+	})
+}
+
+func (c *HeaviestCandidate) registerCommitment(slot iotago.SlotIndex, commitment *Commitment, evictionEvent reactive.Event) {
+	sortedCommitments, slotCreated := c.weightedCommitmentsBySlot.GetOrCreate(slot, c.sortedSetFactory)
 	if slotCreated {
-		c.EvictionEvent(targetSlot).OnTrigger(func() {
-			targetMap.Delete(targetSlot)
-		})
+		evictionEvent.OnTrigger(func() { c.weightedCommitmentsBySlot.Delete(slot) })
 	}
 
-	return weightedCommitments
+	sortedCommitments.Add(commitment)
 }
+
+const (
+	chainSwitchingThreshold iotago.SlotIndex = 10
+
+	measurementOffset iotago.SlotIndex = 1
+)
