7b742d64d1078dba14666b9a1402d9619a5d4e42
jkrvivian <jkrvivian@gmail.com>
2024-01-18 19:45:16 +0800

    Fix linter errors

--- pkg/testsuite/mock/wallet_transactions.go
+++ pkg/testsuite/mock/wallet_transactions.go
@@ -665,6 +665,7 @@ func (w *Wallet) CreateNFTFromInput(transactionName string, inputName string, op
 	)
 }
 
+//nolint:forcetypeassert
 func (w *Wallet) CreateNativeTokenFromInput(transactionName string, inputName string, accountOutputName string) *iotago.SignedTransaction {
 	input := w.Output(inputName)
 	accountOutput := w.AccountOutput(accountOutputName)
