c2f37741fca1c41b7333cb977bc3713cbb354a14
jkrvivian <jkrvivian@gmail.com>
2024-01-19 21:38:09 +0800

    Add TODO comment in AssertTransaction

--- pkg/testsuite/transactions.go
+++ pkg/testsuite/transactions.go
@@ -38,6 +38,8 @@ func (t *TestSuite) AssertTransaction(transaction *iotago.Transaction, node *moc
 			return ierrors.Errorf("AssertTransaction: %s: expected Transaction type %T, got %T", node.Name, transaction, loadedTransactionMetadata.Transaction())
 		}
 
+		// TODO: fix this in another PR
+		//if !assert.Equal(t.fakeTesting, transaction.Outputs, typedTransaction.Outputs) {
 		api := t.DefaultWallet().Node.Protocol.APIForSlot(transactionID.Slot())
 		expected, _ := api.Encode(transaction.Outputs)
 		actual, _ := api.Encode(typedTransaction.Outputs)
