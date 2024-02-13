447d658e905cf5ebd28d7c35554a88ce0888b40d
Philipp Gackstatter <philipp.gackstatter@iota.org>
2024-02-13 07:39:49 +0100

    Fix test due to increased work score params

--- pkg/tests/accounts_test.go
+++ pkg/tests/accounts_test.go
@@ -472,7 +472,7 @@ func Test_NegativeBIC_BlockIssuerLocked(t *testing.T) {
 	node1 := ts.AddValidatorNode("node1")
 	node2 := ts.AddNode("node2")
 
-	wallet1BIC := iotago.BlockIssuanceCredits(100_000)
+	wallet1BIC := iotago.BlockIssuanceCredits(100_000_000)
 	wallet2BIC := iotago.BlockIssuanceCredits(0)
 
 	wallet1 := ts.AddGenesisWallet("wallet 1", node2, testsuite.WithWalletBlockIssuanceCredits(wallet1BIC))
@@ -505,6 +505,9 @@ func Test_NegativeBIC_BlockIssuerLocked(t *testing.T) {
 	// MODIFY EXISTING GENESIS ACCOUNT
 	var block1Slot iotago.SlotIndex = 1
 	var latestParents []iotago.BlockID
+	// The cost of the block that will make the account go negative.
+	// We store it so we can allot that amount later to unlock it.
+	block12ManaCost := iotago.Mana(0)
 	// Issue one block from each of the two block-issuers - one will go negative and the other has enough BICs.
 	{
 		block1Commitment := iotago.NewEmptyCommitment(ts.API)
@@ -512,6 +515,10 @@ func Test_NegativeBIC_BlockIssuerLocked(t *testing.T) {
 		ts.SetCurrentSlot(block1Slot)
 		block11 := ts.IssueBasicBlockWithOptions("block1.1", wallet1, &iotago.TaggedData{}, mock.WithSlotCommitment(block1Commitment))
 		block12 := ts.IssueBasicBlockWithOptions("block1.2", wallet2, &iotago.TaggedData{}, mock.WithStrongParents(block11.ID()), mock.WithSlotCommitment(block1Commitment))
+
+		var err error
+		block12ManaCost, err = block12.ProtocolBlock().ManaCost(block1Commitment.ReferenceManaCost)
+		require.NoError(t, err)
 
 		// Commit BIC burns and check account states.
 		ts.CommitUntilSlot(ts.BlockID("block1.2").Slot(), block12.ID())
@@ -579,7 +586,7 @@ func Test_NegativeBIC_BlockIssuerLocked(t *testing.T) {
 	// Allot some mana to the locked account to unlock it.
 	// The locked wallet 2 is preparing and signs the transaction, but it's issued by wallet 1 whose account is not locked.
 	{
-		allottedBIC := iotago.BlockIssuanceCredits(10_000)
+		allottedBIC := iotago.BlockIssuanceCredits(block12ManaCost)
 		tx1 := wallet2.AllotManaFromInputs("TX1",
 			iotago.Allotments{&iotago.Allotment{
 				AccountID: wallet2.BlockIssuer.AccountID,
