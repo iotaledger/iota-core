879d8d07be52e42061ed054f1c2502527ff5a22a
Daria Dziubałtowska <daria305@gmail.com>
2024-02-21 16:09:03 +0100

    Add BlockRetainer

--- pkg/retainer/retainer.go
+++ pkg/retainer/retainer.go
@@ -7,6 +7,7 @@ import (
 	"github.com/iotaledger/iota.go/v4/api"
 )
 
+// TODO Remove old general interface after merging Andrews PR and connect new block retainer
 // Retainer keeps and resolves all the information needed in the API and INX.
 type Retainer interface {
 	BlockMetadata(blockID iotago.BlockID) (*BlockMetadata, error)
@@ -21,5 +22,22 @@ type Retainer interface {
 	Reset()
 
 	// Interface embeds the required methods of the module.Interface.
+	module.Interface
+}
+
+// BlockRetainer keeps and resolves all the information needed in the API and INX.
+type BlockRetainer interface {
+	BlockMetadata(blockID iotago.BlockID) (*BlockMetadata, error)
+	RetainBlockFailure(*model.Block, api.BlockFailureReason)
+
+	// Interface embeds the required methods of the module.Interface.
+	module.Interface
+}
+
+// TransactionRetainer keeps and resolves all the information needed in the API and INX.
+type TransactionRetainer interface {
+	TransactionMetadata(txID iotago.TransactionID) (*TransactionMetadata, error)
+	RetainTransactionFailure(iotago.TransactionID, error)
+
 	module.Interface
 }
