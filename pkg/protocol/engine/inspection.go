a209ff36851fbb1cabe1bbcc10577e8c251b1feb
Hans Moog <3293976+hmoog@users.noreply.github.com>
2024-03-05 02:15:54 +0100

    Feat: addressed linter issue

--- pkg/protocol/engine/inspection.go
+++ pkg/protocol/engine/inspection.go
@@ -4,7 +4,7 @@ import "github.com/iotaledger/hive.go/runtime/inspection"
 
 // Inspect inspects the Engine and its subcomponents.
 func (e *Engine) Inspect(session ...inspection.Session) inspection.InspectedObject {
-	return inspection.NewInspectedObject(e, func(engine inspection.InspectedObject) {
+	return inspection.NewInspectedObject(e, func(_ inspection.InspectedObject) {
 		// TODO: DUMP ENGINE STRUCTURE IN THE FUTURE
 	}, session...)
 }
